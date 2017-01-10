0b7bd1048f3bb77fcebacd62fc202e94186189d5
Adam Lock <locka99@gmail.com>
2017-01-10 12:29:24 +0000

    Add node opcua sample server

--- tools/3rd-party/node-opcua-client/client.js
+++ tools/3rd-party/node-opcua-client/client.js
@@ -7,132 +7,132 @@ var async = require("async");
 var client = new opcua.OPCUAClient();
 var endpointUrl = "opc.tcp://127.0.0.1:1234/";
 
-
 var the_session, the_subscription;
 
 async.series([
 
-    // step 1 : connect to
-    function(callback)  {
-        client.connect(endpointUrl,function (err) {
-            if(err) {
-                console.log(" cannot connect to endpoint :" , endpointUrl );
-            } else {
-                console.log("connected !");
-            }
-            callback(err);
+        // step 1 : connect to
+        function (callback) {
+            client.connect(endpointUrl, function (err) {
+                if (err) {
+                    console.log(" cannot connect to endpoint :", endpointUrl);
+                } else {
+                    console.log("connected !");
+                }
+                callback(err);
+            });
+        },
+
+        // step 2 : createSession
+        function (callback) {
+            client.createSession(function (err, session) {
+                if (!err) {
+                    the_session = session;
+                }
+                callback(err);
+            });
+        },
+
+        // step 3 : browse
+        function (callback) {
+            the_session.browse("RootFolder", function (err, browse_result) {
+                if (!err) {
+                    browse_result[0].references.forEach(function (reference) {
+                        console.log(reference.browseName.toString());
+                    });
+                }
+                callback(err);
+            });
+        },
+
+        // step 4 : read a variable with readVariableValue
+        function (callback) {
+            the_session.readVariableValue("ns=1;s=free_memory", function (err, dataValue) {
+                if (!err) {
+                    console.log(" free mem % = ", dataValue.toString());
+                }
+                callback(err);
+            });
+
+
+        },
+
+        // step 4' : read a variable with read
+        function (callback) {
+            var max_age = 0;
+            var nodes_to_read = [
+                {nodeId: "ns=1;s=free_memory", attributeId: opcua.AttributeIds.Value}
+            ];
+            the_session.read(nodes_to_read, max_age, function (err, nodes_to_read, dataValues) {
+                if (!err) {
+                    console.log(" free mem % = ", dataValues[0]);
+                }
+                callback(err);
+            });
+
+
+        },
+
+        // step 5: install a subscription and install a monitored item for 10 seconds
+        function (callback) {
+
+            the_subscription = new opcua.ClientSubscription(the_session, {
+                requestedPublishingInterval: 1000,
+                requestedLifetimeCount: 10,
+                requestedMaxKeepAliveCount: 2,
+                maxNotificationsPerPublish: 10,
+                publishingEnabled: true,
+                priority: 10
+            });
+
+            the_subscription.on("started", function () {
+                console.log("subscription started for 2 seconds - subscriptionId=", the_subscription.subscriptionId);
+            }).on("keepalive", function () {
+                console.log("keepalive");
+            }).on("terminated", function () {
+                callback();
+            });
+
+            setTimeout(function () {
+                the_subscription.terminate();
+            }, 10000);
+
+            // install monitored item
+            var monitoredItem = the_subscription.monitor({
+                    nodeId: opcua.resolveNodeId("ns=1;s=free_memory"),
+                    attributeId: opcua.AttributeIds.Value
+                },
+                {
+                    samplingInterval: 100,
+                    discardOldest: true,
+                    queueSize: 10
+                },
+                opcua.read_service.TimestampsToReturn.Both
+            );
+            console.log("-------------------------------------");
+
+            monitoredItem.on("changed", function (dataValue) {
+                console.log(" % free mem = ", dataValue.value.value);
+            });
+        },
+
+        // close session
+        function (callback) {
+            the_session.close(function (err) {
+                if (err) {
+                    console.log("session closed failed ?");
+                }
+                callback();
+            });
+        }
+
+    ],
+    function (err) {
+        if (err) {
+            console.log(" failure ", err);
+        } else {
+            console.log("done!");
+        }
+        client.disconnect(function () {
         });
-    },
-
-    // step 2 : createSession
-    function(callback) {
-        client.createSession( function(err,session) {
-            if(!err) {
-                the_session = session;
-            }
-            callback(err);
-        });
-    },
-
-    // step 3 : browse
-    function(callback) {
-       the_session.browse("RootFolder", function(err,browse_result){
-           if(!err) {
-               browse_result[0].references.forEach(function(reference) {
-                   console.log( reference.browseName.toString());
-               });
-           }
-           callback(err);
-       });
-    },
-
-    // step 4 : read a variable with readVariableValue
-    function(callback) {
-       the_session.readVariableValue("ns=1;s=free_memory", function(err,dataValue) {
-           if (!err) {
-               console.log(" free mem % = " , dataValue.toString());
-           }
-           callback(err);
-       });
-       
-       
-    },
-    
-    // step 4' : read a variable with read
-    function(callback) {
-       var max_age = 0;
-       var nodes_to_read = [
-          { nodeId: "ns=1;s=free_memory", attributeId: opcua.AttributeIds.Value } 
-       ];
-       the_session.read(nodes_to_read, max_age, function(err,nodes_to_read,dataValues) {
-           if (!err) {
-               console.log(" free mem % = " , dataValues[0]);
-           }
-           callback(err);
-       });
-       
-       
-    },
-    
-    // step 5: install a subscription and install a monitored item for 10 seconds
-    function(callback) {
-       
-       the_subscription=new opcua.ClientSubscription(the_session,{
-           requestedPublishingInterval: 1000,
-           requestedLifetimeCount: 10,
-           requestedMaxKeepAliveCount: 2,
-           maxNotificationsPerPublish: 10,
-           publishingEnabled: true,
-           priority: 10
-       });
-       
-       the_subscription.on("started",function(){
-           console.log("subscription started for 2 seconds - subscriptionId=",the_subscription.subscriptionId);
-       }).on("keepalive",function(){
-           console.log("keepalive");
-       }).on("terminated",function(){
-           callback();
-       });
-       
-       setTimeout(function(){
-           the_subscription.terminate();
-       },10000);
-       
-       // install monitored item
-       var monitoredItem  = the_subscription.monitor({
-           nodeId: opcua.resolveNodeId("ns=1;s=free_memory"),
-           attributeId: opcua.AttributeIds.Value
-       },
-       {
-           samplingInterval: 100,
-           discardOldest: true,
-           queueSize: 10
-       },
-       opcua.read_service.TimestampsToReturn.Both
-       );
-       console.log("-------------------------------------");
-       
-       monitoredItem.on("changed",function(dataValue){
-          console.log(" % free mem = ",dataValue.value.value);
-       });
-    },
-
-    // close session
-    function(callback) {
-        the_session.close(function(err){
-            if(err) {
-                console.log("session closed failed ?");
-            }
-            callback();
-        });
-    }
-
-],
-function(err) {
-    if (err) {
-        console.log(" failure ",err);
-    } else {
-        console.log("done!");
-    }
-    client.disconnect(function(){});
-}) ;
+    });
---
+++ tools/3rd-party/node-opcua-server/server.js
@@ -0,0 +1,106 @@
+/*global require,setInterval,console */
+var opcua = require("node-opcua");
+
+
+// Let's create an instance of OPCUAServer
+var server = new opcua.OPCUAServer({
+    port: 4334, // the port of the listening socket of the server
+    resourcePath: "UA/MyLittleServer", // this path will be added to the endpoint resource name
+    buildInfo: {
+        productName: "MySampleServer1",
+        buildNumber: "7658",
+        buildDate: new Date(2014, 5, 2)
+    }
+});
+
+function post_initialize() {
+    console.log("initialized");
+    function construct_my_address_space(server) {
+
+        var addressSpace = server.engine.addressSpace;
+
+        // declare a new object
+        var device = addressSpace.addObject({
+            organizedBy: addressSpace.rootFolder.objects,
+            browseName: "MyDevice"
+        });
+
+        // add some variables
+        // add a variable named MyVariable1 to the newly created folder "MyDevice"
+        var variable1 = 1;
+
+        // emulate variable1 changing every 500 ms
+        setInterval(function () {
+            variable1 += 1;
+        }, 500);
+
+        addressSpace.addVariable({
+            componentOf: device,
+            browseName: "MyVariable1",
+            dataType: "Double",
+            value: {
+                get: function () {
+                    return new opcua.Variant({dataType: opcua.DataType.Double, value: variable1});
+                }
+            }
+        });
+
+        // add a variable named MyVariable2 to the newly created folder "MyDevice"
+        var variable2 = 10.0;
+
+        server.engine.addressSpace.addVariable({
+
+            componentOf: device,
+
+            nodeId: "ns=1;b=1020FFAA", // some opaque NodeId in namespace 4
+
+            browseName: "MyVariable2",
+
+            dataType: "Double",
+
+            value: {
+                get: function () {
+                    return new opcua.Variant({dataType: opcua.DataType.Double, value: variable2});
+                },
+                set: function (variant) {
+                    variable2 = parseFloat(variant.value);
+                    return opcua.StatusCodes.Good;
+                }
+            }
+        });
+        var os = require("os");
+
+        /**
+         * returns the percentage of free memory on the running machine
+         * @return {double}
+         */
+        function available_memory() {
+            // var value = process.memoryUsage().heapUsed / 1000000;
+            var percentageMemUsed = os.freemem() / os.totalmem() * 100.0;
+            return percentageMemUsed;
+        }
+
+        server.engine.addressSpace.addVariable({
+
+            componentOf: device,
+
+            nodeId: "ns=1;s=free_memory", // a string nodeID
+            browseName: "FreeMemory",
+            dataType: "Double",
+            value: {
+                get: function () {
+                    return new opcua.Variant({dataType: opcua.DataType.Double, value: available_memory()});
+                }
+            }
+        });
+    }
+
+    construct_my_address_space(server);
+    server.start(function () {
+        console.log("Server is now listening ... ( press CTRL+C to stop)");
+        console.log("port ", server.endpoints[0].port);
+        var endpointUrl = server.endpoints[0].endpointDescriptions()[0].endpointUrl;
+        console.log(" the primary server endpoint url is ", endpointUrl);
+    });
+}
+server.initialize(post_initialize);
