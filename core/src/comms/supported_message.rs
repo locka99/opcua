99693f99267cf96db40a9a6271a9064e50b7dc44
Adam Lock <locka99@gmail.com>
2017-01-24 22:17:00 +0000

    Add PublishRequest / Response to list of supported types

--- core/src/comms/supported_message.rs
+++ core/src/comms/supported_message.rs
@@ -112,6 +112,12 @@ impl SupportedMessage {
             ObjectId::CreateSubscriptionResponse_Encoding_DefaultBinary => {
                 SupportedMessage::CreateSubscriptionResponse(CreateSubscriptionResponse::decode(stream)?)
             },
+            ObjectId::PublishRequest_Encoding_DefaultBinary => {
+                SupportedMessage::PublishRequest(PublishRequest::decode(stream)?)
+            },
+            ObjectId::PublishResponse_Encoding_DefaultBinary => {
+                SupportedMessage::PublishResponse(PublishResponse::decode(stream)?)
+            },
             _ => {
                 debug!("decoding unsupported for object id {:?}", object_id);
                 SupportedMessage::Invalid(object_id)
