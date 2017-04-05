45bcfc6bf613c3ffb2a6be364b6a71964906bc4c
Adam Lock <locka99@gmail.com>
2017-04-05 20:08:31 +0100

    Improve generation scripts to strip out Xml noise and generate a mod.rs that does away with hand written one (and a macro which stopped IntelliJ autocompleting struct info)

--- tools/schema/gen_types.js
+++ tools/schema/gen_types.js
@@ -6,101 +6,138 @@ var settings = require("./settings");
 
 var types_xml = `${settings.schema_dir}/Opc.Ua.Types.bsd.xml`;
 
-/// This code parses the OPC UA Binary types definitions and creates a generated types.rs file
+/// This code parses the OPC UA Binary types definitions and creates a generated .rs type.
 /// Fields are converted to snake case as they are written. Code for serializing the struct is also generated
 
 /// Any handwritten types are stripped from the output
 
 var ignored_types =
-    [
-        "ExtensionObject", "DataValue", "LocalizedText", "QualifiedName", "DiagnosticInfo", "Variant",
-        "ExpandedNodeId", "NodeId", "ByteStringNodeId", "GuidNodeId", "StringNodeId", "NumericNodeId",
-        "FourByteNodeId", "TwoByteNodeId", "XmlElement", "Union", "RequestHeader", "ResponseHeader", "ExtensionObject",
-    ];
+  [
+    "ExtensionObject", "DataValue", "LocalizedText", "QualifiedName", "DiagnosticInfo", "Variant",
+    "ExpandedNodeId", "NodeId", "ByteStringNodeId", "GuidNodeId", "StringNodeId", "NumericNodeId",
+    "FourByteNodeId", "TwoByteNodeId", "XmlElement", "Union", "RequestHeader", "ResponseHeader", "ExtensionObject",
+    "Node", "InstanceNode", "TypeNode", "ObjectNode", "ObjectTypeNode", "VariableNode", "VariableTypeNode", "ReferenceTypeNode",
+    "MethodNode", "ViewNode", "DataTypeNode", "ReferenceNode",
+    // Excluded because they use unimplemented enums
+    "ModificationInfo", "HistoryModifiedData", "UpdateDataDetails", "UpdateEventDetails", "UpdateStructureDataDetails", "RedundantServerDataType",
+    "ServerStatusDataType", "AxisInformation"
+  ];
 
 function convertFieldName(name) {
-    // Convert field name to snake case
-    return _.snakeCase(name);
+  // Convert field name to snake case
+  return _.snakeCase(name);
 }
 
 var parser = new xml2js.Parser();
 fs.readFile(types_xml, function (err, data) {
-    parser.parseString(data, function (err, result) {
-        var data = {
-            structured_types: []
-        };
+  parser.parseString(data, function (err, result) {
+    var data = {
+      structured_types: []
+    };
 
-        var structured_types = result["opc:TypeDictionary"]["opc:StructuredType"];
-        _.each(structured_types, function (structured_type_element) {
+    var structured_types = result["opc:TypeDictionary"]["opc:StructuredType"];
+    _.each(structured_types, function (structured_type_element) {
 
-            var name = structured_type_element["$"]["Name"];
-            // if name in ignored_types, do nothing
-            if (!_.includes(ignored_types, name)) {
-                var fields_to_add = [];
-                var fields_to_hide = [];
-                _.each(structured_type_element["opc:Field"], function (field) {
-                    // Convert field name to snake case
-                    var field_name = convertFieldName(field["$"]["Name"]);
+      var name = structured_type_element["$"]["Name"];
+      // if name in ignored_types, do nothing
+      if (!_.includes(ignored_types, name)) {
+        var fields_to_add = [];
+        var fields_to_hide = [];
+        _.each(structured_type_element["opc:Field"], function (field) {
+          // Convert field name to snake case
+          var field_name = convertFieldName(field["$"]["Name"]);
 
-                    // Strip namespace off the type
-                    var type = field["$"]["TypeName"].split(":")[1];
+          // Strip namespace off the type
+          var type = field["$"]["TypeName"].split(":")[1];
 
-                    // Replace String with UAString
-                    if (type === "String") {
-                        type = "UAString";
-                    }
+          // Replace String with UAString
+          if (type === "String") {
+            type = "UAString";
+          }
 
-                    // Look for arrays
-                    if (_.has(field["$"], "LengthField")) {
-                        fields_to_add.push({
-                            name: field_name,
-                            type: `Option<Vec<${type}>>`,
-                            inner_type: type,
-                            is_array: true,
-                        })
-                        fields_to_hide.push(convertFieldName(field["$"]["LengthField"]));
-                    }
-                    else {
-                        fields_to_add.push({
-                            name: field_name,
-                            type: type,
-                        })
-                    }
-                })
+          // Look for arrays
+          if (_.has(field["$"], "LengthField")) {
+            fields_to_add.push({
+              name: field_name,
+              type: `Option<Vec<${type}>>`,
+              inner_type: type,
+              is_array: true,
+            })
+            fields_to_hide.push(convertFieldName(field["$"]["LengthField"]));
+          }
+          else {
+            fields_to_add.push({
+              name: field_name,
+              type: type,
+            })
+          }
+        })
 
-                var structured_type = {
-                    name: name,
-                    fields_to_add: fields_to_add,
-                    fields_to_hide: fields_to_hide,
-                }
-                if (_.has(structured_type_element, "opc:Documentation")) {
-                    structured_type.documentation = structured_type_element["opc:Documentation"];
-                }
-                if (_.has(structured_type_element["$"], "BaseType")) {
-                    structured_type.base_type = structured_type_element["$"]["BaseType"];
-                }
-                data.structured_types.push(structured_type)
-            }
+        var structured_type = {
+          name: name,
+          fields_to_add: fields_to_add,
+          fields_to_hide: fields_to_hide,
+        }
+        if (_.has(structured_type_element, "opc:Documentation")) {
+          structured_type.documentation = structured_type_element["opc:Documentation"];
+        }
+        if (_.has(structured_type_element["$"], "BaseType")) {
+          structured_type.base_type = structured_type_element["$"]["BaseType"];
+        }
+        data.structured_types.push(structured_type)
+      }
 
-        });
-        generate_types(data);
     });
+    generate_types(data);
+  });
 });
 
+function write_to_file(file_path, contents) {
+  var buffer = new Buffer(contents);
+  var fd = fs.openSync(file_path, 'w');
+  fs.writeSync(fd, buffer, 0, buffer.length, null);
+  fs.closeSync(fd);
+}
+
 function generate_types(data) {
-    // Output structured types
-    _.each(data.structured_types, function (structured_type) {
-        generate_structured_type_file(structured_type);
-    });
+  // Output structured types
+  _.each(data.structured_types, function (structured_type) {
+    generate_structured_type_file(structured_type);
+  });
+  generate_types_mod(data.structured_types);
+}
+
+function generate_types_mod(structured_types) {
+  var file_name = "mod.rs";
+  var file_path = `${settings.rs_dir}/types/generated/${file_name}`
+
+  var contents = `// This file was autogenerated from Opc.Ua.Types.bsd.xml
+// DO NOT EDIT THIS FILE
+
+mod node_ids;
+pub use self::node_ids::*;
+mod status_codes;
+pub use self::status_codes::*;
+
+// All of the serializable types follow
+`;
+  _.each(structured_types, function (structured_type) {
+    var mod_name = _.snakeCase(structured_type.name);
+    contents += `mod ${mod_name};
+pub use self::${mod_name}::*;
+`
+  });
+
+  write_to_file(file_path, contents);
 }
 
 function generate_structured_type_file(structured_type) {
-    var file_name = _.snakeCase(structured_type.name) + ".rs";
-    var file_path = `${settings.rs_dir}/types/generated/${file_name}`
+  var file_name = _.snakeCase(structured_type.name) + ".rs";
+  var file_path = `${settings.rs_dir}/types/generated/${file_name}`
 
-    console.log("Creating structured type file - " + file_path);
+  console.log("Creating structured type file - " + file_path);
 
-    var contents = `// This file was autogenerated from Opc.Ua.Types.bsd.xml
+  var contents = `// This file was autogenerated from Opc.Ua.Types.bsd.xml
 // DO NOT EDIT THIS FILE
 
 use std::io::{Read, Write};
@@ -111,113 +148,110 @@ use types::*;
 use services::*;
 
 `;
-    if (_.has(structured_type, "documentation")) {
-        contents += `/// ${structured_type.documentation}\n`;
-    }
-    contents += `#[derive(Debug, Clone, PartialEq)]
+  if (_.has(structured_type, "documentation")) {
+    contents += `/// ${structured_type.documentation}\n`;
+  }
+  contents += `#[derive(Debug, Clone, PartialEq)]
 pub struct ${structured_type.name} {
 `;
-    _.each(structured_type.fields_to_add, function (field) {
-        if (!_.includes(structured_type.fields_to_hide, field.name)) {
-            contents += `    pub ${field.name}: ${field.type},\n`;
-        }
-    });
-    contents += `}
+  _.each(structured_type.fields_to_add, function (field) {
+    if (!_.includes(structured_type.fields_to_hide, field.name)) {
+      contents += `    pub ${field.name}: ${field.type},\n`;
+    }
+  });
+  contents += `}
 
 `;
 
-    if (_.has(structured_type, "base_type") && structured_type.base_type === "ua:ExtensionObject") {
-        contents += `impl MessageInfo for ${structured_type.name} {
+  if (_.has(structured_type, "base_type") && structured_type.base_type === "ua:ExtensionObject") {
+    contents += `impl MessageInfo for ${structured_type.name} {
     fn object_id(&self) -> ObjectId {
         ObjectId::${structured_type.name}_Encoding_DefaultBinary
     }
 }
 
 `;
-    }
+  }
 
-    contents += `impl BinaryEncoder<${structured_type.name}> for ${structured_type.name} {
+  contents += `impl BinaryEncoder<${structured_type.name}> for ${structured_type.name} {
     fn byte_len(&self) -> usize {
 `;
-    if (structured_type.fields_to_add.length > 0) {
-        contents += `        let mut size = 0;\n`;
+  if (structured_type.fields_to_add.length > 0) {
+    contents += `        let mut size = 0;\n`;
 
-        _.each(structured_type.fields_to_add, function (field) {
-            if (!_.includes(structured_type.fields_to_hide, field.name)) {
-                if (_.has(field, 'is_array')) {
-                    contents += `        size += byte_len_array(&self.${field.name});\n`;
-                }
-                else {
-                    contents += `        size += self.${field.name}.byte_len();\n`;
-                }
-            }
-        });
+    _.each(structured_type.fields_to_add, function (field) {
+      if (!_.includes(structured_type.fields_to_hide, field.name)) {
+        if (_.has(field, 'is_array')) {
+          contents += `        size += byte_len_array(&self.${field.name});\n`;
+        }
+        else {
+          contents += `        size += self.${field.name}.byte_len();\n`;
+        }
+      }
+    });
 
-        contents += `        size\n`;
-    }
-    else {
-        contents += `        0\n`;
-    }
+    contents += `        size\n`;
+  }
+  else {
+    contents += `        0\n`;
+  }
 
-    contents += `    }
+  contents += `    }
 
     #[allow(unused_variables)]
     fn encode<S: Write>(&self, stream: &mut S) -> EncodingResult<usize> {
 `;
 
-    if (structured_type.fields_to_add.length > 0) {
-        contents += `        let mut size = 0;\n`;
+  if (structured_type.fields_to_add.length > 0) {
+    contents += `        let mut size = 0;\n`;
 
-        _.each(structured_type.fields_to_add, function (field) {
-            if (!_.includes(structured_type.fields_to_hide, field.name)) {
-                if (_.has(field, 'is_array')) {
-                    contents += `        size += write_array(stream, &self.${field.name})?;\n`;
-                }
-                else {
-                    contents += `        size += self.${field.name}.encode(stream)?;\n`;
-                }
-            }
-        });
+    _.each(structured_type.fields_to_add, function (field) {
+      if (!_.includes(structured_type.fields_to_hide, field.name)) {
+        if (_.has(field, 'is_array')) {
+          contents += `        size += write_array(stream, &self.${field.name})?;\n`;
+        }
+        else {
+          contents += `        size += self.${field.name}.encode(stream)?;\n`;
+        }
+      }
+    });
 
-        contents += `        Ok(size)\n`;
-    }
-    else {
-        contents += `        Ok(0)\n`;
-    }
+    contents += `        Ok(size)\n`;
+  }
+  else {
+    contents += `        Ok(0)\n`;
+  }
 
-    contents += `    }
+  contents += `    }
 
     #[allow(unused_variables)]
     fn decode<S: Read>(stream: &mut S) -> EncodingResult<Self> {
 `;
 
-    _.each(structured_type.fields_to_add, function (field) {
-        if (!_.includes(structured_type.fields_to_hide, field.name)) {
-            if (_.has(field, 'is_array')) {
-                contents += `        let ${field.name}: ${field.type} = read_array(stream)?;\n`;
-            }
-            else {
-                contents += `        let ${field.name} = ${field.type}::decode(stream)?;\n`;
-            }
-        }
-    });
+  _.each(structured_type.fields_to_add, function (field) {
+    if (!_.includes(structured_type.fields_to_hide, field.name)) {
+      if (_.has(field, 'is_array')) {
+        contents += `        let ${field.name}: ${field.type} = read_array(stream)?;\n`;
+      }
+      else {
+        contents += `        let ${field.name} = ${field.type}::decode(stream)?;\n`;
+      }
+    }
+  });
 
-    contents += `        Ok(${structured_type.name} {
+  contents += `        Ok(${structured_type.name} {
 `;
 
-    _.each(structured_type.fields_to_add, function (field) {
-        if (!_.includes(structured_type.fields_to_hide, field.name)) {
-            contents += `            ${field.name}: ${field.name},\n`;
-        }
-    });
+  _.each(structured_type.fields_to_add, function (field) {
+    if (!_.includes(structured_type.fields_to_hide, field.name)) {
+      contents += `            ${field.name}: ${field.name},\n`;
+    }
+  });
 
-    contents += `        })
+  contents += `        })
     }
 }
 `;
 
-    var buffer = new Buffer(contents);
-    var fd = fs.openSync(file_path, 'w');
-    fs.writeSync(fd, buffer, 0, buffer.length, null);
-    fs.closeSync(fd);
+  write_to_file(file_path, contents);
 }
