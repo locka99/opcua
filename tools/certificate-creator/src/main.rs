445f6b9ef741af7f11b398898f312bfd8fe56fba
Adam Lock <locka99@gmail.com>
2017-05-01 23:58:40 +0100

    Stub openssl calls

--- tools/certificate-creator/src/main.rs
+++ tools/certificate-creator/src/main.rs
@@ -3,10 +3,30 @@ extern crate openssl;
 
 #[cfg(feature = "crypto")]
 fn main() {
-    use openssl::*;
-    // TODO This is a placeholder, where the certificate creator will generate a certificate compatible
-    // with OPC UA for Rust.
+    use openssl::ssl::*;
+    use openssl::x509::*;
 
+    let mut builder: X509Builder = X509Builder::new().unwrap();
+
+    /// use openssl::x509::{X509, X509NameBuilder};
+
+    let mut issuer_name = X509NameBuilder::new().unwrap();
+    // Common name
+    issuer_name.append_entry_by_text("CN", "www.example.com").unwrap();
+    // Organization
+    issuer_name.append_entry_by_text("O", "Some organization").unwrap();
+    // Organization Unit
+    //..
+
+    // Country
+    issuer_name.append_entry_by_text("C", "US").unwrap();
+    // State
+    issuer_name.append_entry_by_text("ST", "CA").unwrap();
+    let issuer_name = issuer_name.build();
+
+    builder.set_issuer_name(&issuer_name);
+
+    let cert = builder.build();
 }
 
 #[cfg(not(feature = "crypto"))]
