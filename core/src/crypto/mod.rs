067d29b8b853c7608bbc0236adc6118d188f0ee7
Adam Lock <locka99@gmail.com>
2017-05-03 00:02:53 +0100

    Some minor shaping of crypto interface

--- core/src/crypto/mod.rs
+++ core/src/crypto/mod.rs
@@ -10,6 +10,21 @@ use std::path::*;
 
 mod cert_manager;
 
+
+#[derive(Debug)]
+pub struct X509KeyArgs {
+    pub key_size: u32,
+    pub pki_path: String,
+    pub overwrite: bool,
+    pub common_name: String,
+    pub organization: String,
+    pub organizational_unit: String,
+    pub country: String,
+    pub state: String,
+    pub alt_host_names: Vec<String>,
+    pub certificate_duration_days: u32,
+}
+
 trait Crypto {
     // This function specifies the crypto capabilities of the compiled software. The value false
     // means the software has no crypto capability so calling other functions below is a waste of time
@@ -17,21 +32,84 @@ trait Crypto {
     fn is_crypto_enabled() -> bool;
 
     // Creates an asymmetric key/pair
-    fn create_key_pair(public_key_path: &Path, private_key_path: &Path) -> Result<(), ()>;
+    fn create_key_pair(args: &X509KeyArgs) -> Result<(), ()>;
 
     // Validates that the certificate is trusted by the server /client
-    fn is_certificate_trusted(public_key_path: &Path);
+    fn is_certificate_trusted(public_key_path: &Path) -> Result<bool, ()>;
 
     // Decrypts bytes of data using the specified key
-    fn decrypt_bytes();
+    fn decrypt_bytes(bytes: &[u8], key: &[u8]) -> Result<Vec<u8>, ()>;
 
     // Verifies the specified data using the specified key
-    fn verify_signature(data: &[u8], signature: &[u8]);
+    fn verify_signature(data: &[u8], signature: &[u8]) -> Result<bool, ()>;
 
     // Encrypts bytes using the specified key
-    fn encrypt_bytes(data: &[u8], key: &[u8]) -> Vec<u8>;
+    fn encrypt_bytes(data: &[u8], key: &[u8]) -> Result<Vec<u8>, ()>;
 
     // Signs bytes using the specified key
-    fn sign_bytes(data: &[u8], key: &[u8]) -> Vec<u8>;
+    fn sign_bytes(data: &[u8], key: &[u8]) -> Result<Vec<u8>, ()>;
 }
 
+struct NullCrypto {}
+
+impl Crypto for NullCrypto {
+    fn is_crypto_enabled() -> bool {
+        false
+    }
+
+    fn create_key_pair(args: &X509KeyArgs) -> Result<(), ()> {
+        Err(())
+    }
+
+    fn is_certificate_trusted(public_key_path: &Path) -> Result<bool, ()> {
+        Err(())
+    }
+
+    fn decrypt_bytes(bytes: &[u8], key: &[u8]) -> Result<Vec<u8>, ()> {
+        Err(())
+    }
+
+    fn verify_signature(data: &[u8], signature: &[u8]) -> Result<bool, ()> {
+        Err(())
+    }
+
+    fn encrypt_bytes(data: &[u8], key: &[u8]) -> Result<Vec<u8>, ()> {
+        Err(())
+    }
+
+    fn sign_bytes(data: &[u8], key: &[u8]) -> Result<Vec<u8>, ()> {
+        Err(())
+    }
+}
+
+pub struct OpenSSLCrypto {}
+
+impl Crypto for OpenSSLCrypto {
+    fn is_crypto_enabled() -> bool {
+        true
+    }
+
+    fn create_key_pair(args: &X509KeyArgs) -> Result<(), ()> {
+        Err(())
+    }
+
+    fn is_certificate_trusted(public_key_path: &Path) -> Result<bool, ()> {
+        Err(())
+    }
+
+    fn decrypt_bytes(bytes: &[u8], key: &[u8]) -> Result<Vec<u8>, ()> {
+        Err(())
+    }
+
+    fn verify_signature(data: &[u8], signature: &[u8]) -> Result<bool, ()> {
+        Err(())
+    }
+
+    fn encrypt_bytes(data: &[u8], key: &[u8]) -> Result<Vec<u8>, ()> {
+        Err(())
+    }
+
+    fn sign_bytes(data: &[u8], key: &[u8]) -> Result<Vec<u8>, ()> {
+        Err(())
+    }
+}
