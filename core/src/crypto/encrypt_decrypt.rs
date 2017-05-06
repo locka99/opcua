5343345476ec13f6396d5ab1d6a97f0a4c36431d
Adam Lock <locka99@gmail.com>
2017-05-06 21:16:57 +0100

    Crypto work

--- core/src/crypto/encrypt_decrypt.rs
+++ core/src/crypto/encrypt_decrypt.rs
@@ -1,16 +1,64 @@
 use openssl::aes::*;
 use openssl::symm::Mode;
 
-/// Encrypts data using AES. The initialization vector should be supplied and comes
-/// from the OPC UA secure channel
-pub fn encrypt_aes(inx: &[u8], out: &mut [u8], iv: &[u8], key: &AesKey) -> Result<(), ()> {
-    //    aes_ige(inx, out, key, iv, Mode::Encrypt);
-    Err(())
+fn validate_aes_args(inx: &[u8], out: &mut [u8], nonce: &[u8], key: &AesKey) -> Result<(), String> {
+    if inx.len() != out.len() {
+        Err(format!("In and out buffers have different lengths {} vs {}", inx.len(), out.len()))
+    } else if inx.len() % 16 != 0 {
+        // Works for out too because inx.len == out.len
+        Err(format!("In and out buffers are not 16-byte padded, len = {}", inx.len()))
+    } else if nonce.len() != 16 && nonce.len() != 32 {
+        // ... It would be nice to compare nonce size to be exact to the key size here (should be the
+        // same) but AesKey doesn't tell us that info. Have to check elsewhere
+        Err(format!("Nonce is not an expected size, len = {}", nonce.len()))
+    } else {
+        Ok(())
+    }
 }
 
-/// Encrypts data using AES. The initialization vector should be supplied and comes
-/// from the OPC UA secure channel
-pub fn decrypt_aes(inx: &[u8], out: &mut [u8], iv: &[u8], key: &AesKey) -> Result<(), ()> {
-    //    aes_ige(inx, out, key, iv, Mode::Decrypt);
-    Err(())
+
+/// Encrypts data using AES. The initialization vector is the nonce generated for the secure channel.
+/// The key can be 128, 160 or 256bits.
+pub fn encrypt_aes(inx: &[u8], out: &mut [u8], nonce: &mut [u8], key: &AesKey) -> Result<(), String> {
+    let _ = validate_aes_args(inx, out, nonce, key)?;
+    aes_ige(inx, out, key, nonce, Mode::Encrypt);
+    Ok(())
+}
+
+/// Encrypts data using AES. The initialization vector is the nonce generated for the secure channel
+pub fn decrypt_aes(inx: &[u8], out: &mut [u8], nonce: &mut [u8], key: &AesKey) -> Result<(), String> {
+    let _ = validate_aes_args(inx, out, nonce, key)?;
+    aes_ige(inx, out, key, nonce, Mode::Decrypt);
+    Ok(())
+}
+
+#[test]
+fn aes_test() {
+    // Multiple of 16
+    let mut plaintext = b"0123456789012345";
+    let mut ciphertext: [u8; 16] = [0; 16];
+
+    use rand::{self, Rng};
+    let mut rng = rand::thread_rng();
+    let mut key = vec![0u8; 16];
+    rng.fill_bytes(&mut key);
+
+    let mut nonce = vec![0u8; 16];
+    rng.fill_bytes(&mut nonce);
+
+    {
+        let key = AesKey::new_encrypt(&key).unwrap();
+        let r = encrypt_aes(plaintext, &mut ciphertext, &mut nonce, &key);
+        assert!(r.is_ok());
+    }
+
+    let mut plaintext2: [u8; 16] = [0; 16];
+
+    {
+        let key = AesKey::new_decrypt(&key).unwrap();
+        let r = decrypt_aes(&ciphertext, &mut plaintext2, &mut nonce, &key);
+        assert!(r.is_ok());
+    }
+
+    assert_eq!(&plaintext[..], &plaintext2[..]);
 }
--- core/src/crypto/mod.rs
+++ core/src/crypto/mod.rs
@@ -22,60 +22,6 @@ pub struct X509CreateCertArgs {
     pub certificate_duration_days: u32,
 }
 
-/// Tests if crypto is enabled, true for yes it is otherwise false
-pub fn is_crypto_enabled() -> bool {
-    cfg!(feature = "crypto")
-}
-
-trait Crypto {
-    // Creates an asymmetric key/pair
-    fn create_key_pair(args: X509CreateCertArgs) -> Result<(), String>;
-
-    // Validates that the certificate is trusted by the server /client
-    fn is_certificate_trusted(public_key_path: &Path) -> Result<bool, ()>;
-
-    // Decrypts bytes of data using the specified key
-    fn decrypt_bytes(data: &[u8], key: &[u8]) -> Result<Vec<u8>, ()>;
-
-    // Verifies the specified data using the specified key
-    fn verify_signature(data: &[u8], signature: &[u8]) -> Result<bool, ()>;
-
-    // Encrypts bytes using the specified key
-    fn encrypt_bytes(data: &[u8], key: &[u8]) -> Result<Vec<u8>, ()>;
-
-    // Signs bytes using the specified key
-    fn sign_bytes(data: &[u8], key: &[u8]) -> Result<Vec<u8>, ()>;
-}
-
-#[cfg(not(feature = "crypto"))]
-pub struct NullCrypto {}
-
-#[cfg(not(feature = "crypto"))]
-impl Crypto for NullCrypto {
-    fn create_key_pair(_: X509CreateCertArgs) -> Result<(), String> {
-        panic!("Crypto is disabled");
-    }
-
-    fn is_certificate_trusted(_: &Path) -> Result<bool, ()> {
-        panic!("Crypto is disabled");
-    }
-
-    fn decrypt_bytes(_: &[u8], _: &[u8]) -> Result<Vec<u8>, ()> {
-        panic!("Crypto is disabled");
-    }
-
-    fn verify_signature(_: &[u8], _: &[u8]) -> Result<bool, ()> {
-        panic!("Crypto is disabled");
-    }
-
-    fn encrypt_bytes(_: &[u8], _: &[u8]) -> Result<Vec<u8>, ()> {
-        panic!("Crypto is disabled");
-    }
-
-    fn sign_bytes(_: &[u8], _: &[u8]) -> Result<Vec<u8>, ()> {
-        panic!("Crypto is disabled");
-    }
-}
 
 #[cfg(feature = "crypto")]
 pub mod cert_manager;
@@ -86,32 +32,131 @@ pub mod sign_verify;
 #[cfg(feature = "crypto")]
 pub mod encrypt_decrypt;
 
-#[cfg(feature = "crypto")]
-pub struct RealCrypto {}
+/// Tests if crypto is enabled, true for yes it is otherwise false
+pub fn is_crypto_enabled() -> bool {
+    cfg!(feature = "crypto")
+}
+
+trait Crypto {
+    // Validates that the certificate is trusted by the server /client
+    fn is_certificate_trusted(public_key_path: &Path) -> Result<bool, ()>;
+
+    // Encrypts bytes using the specified key
+    fn symmetric_encrypt(data: &[u8], key: &[u8]) -> Result<Vec<u8>, ()>;
+
+    // Decrypts bytes of data using the specified key
+    fn symmetric_decrypt(data: &[u8], key: &[u8]) -> Result<Vec<u8>, ()>;
+
+    // Encrypts bytes using the specified key
+    fn asymmetric_encrypt(data: &[u8], key: &[u8]) -> Result<Vec<u8>, ()>;
+
+    // Decrypts bytes of data using the specified key
+    fn asymmetric_decrypt(data: &[u8], key: &[u8]) -> Result<Vec<u8>, ()>;
+
+    // Signs bytes using the specified key
+    fn sign_bytes(data: &[u8], key: &[u8]) -> Result<Vec<u8>, ()>;
+
+    // Verifies the specified data using the specified key
+    fn verify_signature(data: &[u8], signature: &[u8]) -> Result<bool, ()>;
+}
+
+// 128Rsa15
+//
+// A suite of algorithms that uses RSA15 as Key-Wrap-algorithm and 128-Bit for encryption algorithms.
+//
+// -> SymmetricSignatureAlgorithm – HmacSha1 – (http://www.w3.org/2000/09/xmldsig#hmac-sha1).
+// -> SymmetricEncryptionAlgorithm – Aes128 – (http://www.w3.org/2001/04/xmlenc#aes128-cbc).
+// -> AsymmetricSignatureAlgorithm – RsaSha1 – (http://www.w3.org/2000/09/xmldsig#rsa-sha1).
+// -> AsymmetricKeyWrapAlgorithm – KwRsa15 – (http://www.w3.org/2001/04/xmlenc#rsa-1_5).
+// -> AsymmetricEncryptionAlgorithm – Rsa15 – (http://www.w3.org/2001/04/xmlenc#rsa-1_5).
+// -> KeyDerivationAlgorithm – PSha1 – (http://docs.oasis-open.org/ws-sx/ws-secureconversation/200512/dk/p_sha1).
+// -> DerivedSignatureKeyLength – 128.
+// -> MinAsymmetricKeyLength – 1024
+// -> MaxAsymmetricKeyLength – 2048
+// -> CertificateSignatureAlgorithm – Sha1
+//
+// If a certificate or any certificate in the chain is not signed with a hash that is Sha1 or stronger then the certificate shall be rejected.
 
 #[cfg(feature = "crypto")]
-impl Crypto for RealCrypto {
-    fn create_key_pair(args: X509CreateCertArgs) -> Result<(), String> {
-        cert_manager::CertificateStore::create_cert(args)
-    }
+pub struct Crypto128Rsa15 {}
 
+#[cfg(feature = "crypto")]
+impl Crypto for Crypto128Rsa15 {
     fn is_certificate_trusted(_: &Path) -> Result<bool, ()> {
         unimplemented!();
     }
 
-    fn decrypt_bytes(_: &[u8], _: &[u8]) -> Result<Vec<u8>, ()> {
+    // -> SymmetricEncryptionAlgorithm – Aes128 – (http://www.w3.org/2001/04/xmlenc#aes128-cbc).
+    fn symmetric_decrypt(_: &[u8], _: &[u8]) -> Result<Vec<u8>, ()> {
         unimplemented!();
     }
 
+    // -> SymmetricEncryptionAlgorithm – Aes128 – (http://www.w3.org/2001/04/xmlenc#aes128-cbc).
+    fn symmetric_encrypt(_: &[u8], _: &[u8]) -> Result<Vec<u8>, ()> {
+        unimplemented!();
+    }
+
+    // Encrypts bytes using the specified key
+    fn asymmetric_encrypt(data: &[u8], key: &[u8]) -> Result<Vec<u8>, ()> {
+        unimplemented!();
+    }
+
+    // Decrypts bytes of data using the specified key
+    fn asymmetric_decrypt(data: &[u8], key: &[u8]) -> Result<Vec<u8>, ()> {
+        unimplemented!();
+    }
+
+    // SymmetricSignatureAlgorithm – HmacSha1 – (http://www.w3.org/2000/09/xmldsig#hmac-sha1).
     fn verify_signature(_: &[u8], _: &[u8]) -> Result<bool, ()> {
         unimplemented!();
     }
 
-    fn encrypt_bytes(_: &[u8], _: &[u8]) -> Result<Vec<u8>, ()> {
-        unimplemented!();
-    }
-
+    // SymmetricSignatureAlgorithm – HmacSha1 – (http://www.w3.org/2000/09/xmldsig#hmac-sha1).
     fn sign_bytes(_: &[u8], _: &[u8]) -> Result<Vec<u8>, ()> {
         unimplemented!();
     }
 }
+
+// Security Basic 256
+//
+// A suite of algorithms that are for 256-Bit encryption, algorithms include:
+//
+// -> SymmetricSignatureAlgorithm – HmacSha1 – (http://www.w3.org/2000/09/xmldsig#hmac-sha1).
+// -> SymmetricEncryptionAlgorithm – Aes256 – (http://www.w3.org/2001/04/xmlenc#aes256-cbc).
+// -> AsymmetricSignatureAlgorithm – RsaSha1 – (http://www.w3.org/2000/09/xmldsig#rsa-sha1).
+// -> AsymmetricKeyWrapAlgorithm – KwRsaOaep – (http://www.w3.org/2001/04/xmlenc#rsa-oaep-mgf1p).
+// -> AsymmetricEncryptionAlgorithm – RsaOaep – (http://www.w3.org/2001/04/xmlenc#rsa-oaep).
+// -> KeyDerivationAlgorithm – PSha1 – (http://docs.oasis-open.org/ws-sx/ws-secureconversation/200512/dk/p_sha1).
+// -> DerivedSignatureKeyLength – 192.
+// -> MinAsymmetricKeyLength – 1024
+// -> MaxAsymmetricKeyLength – 2048
+// -> CertificateSignatureAlgorithm –
+//
+// Sha1 [deprecated] or Sha256 [recommended]
+//
+// If a certificate or any certificate in the chain is not signed with a hash that is Sha1 or stronger then the certificate shall be rejected.
+// Release 1.03 17 OPC Unified Architecture, Part 7
+// Both Sha1 and Sha256 shall be supported. However, it is recommended to use Sha256 since Sha1 is considered not secure anymore.
+
+// Security Basic 256 Sha256
+//
+// A suite of algorithms that are for 256-Bit encryption, algorithms include.
+//
+// -> SymmetricSignatureAlgorithm – Hmac_Sha256 – (http://www.w3.org/2000/09/xmldsig#hmac-sha256).
+// -> SymmetricEncryptionAlgorithm – Aes256_CBC – (http://www.w3.org/2001/04/xmlenc#aes256-cbc).
+// -> AsymmetricSignatureAlgorithm – Rsa_Sha256 – (http://www.w3.org/2001/04/xmldsig#rsa-sha256).
+// -> AsymmetricKeyWrapAlgorithm – KwRsaOaep – (http://www.w3.org/2001/04/xmlenc#rsa-oaep-mgf1p).
+// -> AsymmetricEncryptionAlgorithm – Rsa_Oaep – (http://www.w3.org/2001/04/xmlenc#rsa-oaep).
+// -> KeyDerivationAlgorithm – PSHA256 – (http://docs.oasis-open.org/ws-sx/ws-secureconversation/200512/dk/p_sha256).
+// -> DerivedSignatureKeyLength – 256
+// -> MinAsymmetricKeyLength – 2048
+// -> MaxAsymmetricKeyLength – 4096
+// -> CertificateSignatureAlgorithm – Sha256
+//
+// If a certificate or any certificate in the chain is not signed with a hash that is Sha256 or stronger
+// then the certificate shall be rejected. Support for this security profile may require support for
+// a second application instance certificate, with a larger keysize. Applications shall support
+// multiple Application Instance Certificates if required by supported Security Polices and use
+// the certificate that is required for a given security endpoint.
+
+
