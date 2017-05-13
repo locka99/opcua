31cd60aa186f6490f0439d009adfc385e31c85e0
Adam Lock <locka99@gmail.com>
2017-05-13 14:40:34 +0100

    Remove fuzz (only works on Linux)

--- fuzz/fuzzers/fuzzer_script_1.rs
+++
@@ -1,8 +0,0 @@
-#![no_main]
-#[macro_use] extern crate libfuzzer_sys;
-extern crate opcua_core;
-
-fuzz_target!(|data: &[u8]| {
-    // fuzzed code goes here
-    // TODO test chunking behavior here~
-});
