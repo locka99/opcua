801f0037ab42aa42a7fb4caa0f51a4b487f37d8c
Adam Lock <locka99@gmail.com>
2018-04-10 22:32:23 +0100

    Remove some warnings

--- integration/src/main.rs
+++ integration/src/main.rs
@@ -1,6 +1,5 @@
 extern crate chrono;
 extern crate futures;
-#[macro_use]
 extern crate log;
 extern crate opcua_client;
 extern crate opcua_core;
@@ -8,7 +7,7 @@ extern crate opcua_server;
 extern crate opcua_types;
 
 fn main() {
-    panic!("I do not do anything yet, run with cargo test");
+    panic!("I do not do anything, run with cargo test");
 }
 
 #[cfg(test)]
