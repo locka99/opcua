be124b9c64c2f363529babf3f6c5e4fd5c1a40cb
Adam Lock <locka99@gmail.com>
2018-04-11 23:09:53 +0100

    Fix compile error

--- integration/src/main.rs
+++ integration/src/main.rs
@@ -1,5 +1,6 @@
 extern crate chrono;
 extern crate futures;
+#[macro_use]
 extern crate log;
 extern crate opcua_client;
 extern crate opcua_core;
@@ -7,6 +8,7 @@ extern crate opcua_server;
 extern crate opcua_types;
 
 fn main() {
+    trace!("Needs to be run with cargo test");
     panic!("I do not do anything, run with cargo test");
 }
 
