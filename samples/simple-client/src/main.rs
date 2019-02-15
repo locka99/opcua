04409b9624352c50550fa459a752842e377e5cfe
Adam Lock <locka99@gmail.com>
2019-02-15 23:24:33 +0000

    Make simple-client simpler by not having a dual mode to it. Just subscribe

--- samples/simple-client/src/main.rs
+++ samples/simple-client/src/main.rs
@@ -13,7 +13,7 @@ use opcua_client::prelude::*;
 
 fn main() {
     // Read command line arguments
-    let (subscribe, url) = {
+    let url = {
         let m = App::new("Simple OPC UA Client")
             .arg(Arg::with_name("url")
                 .long("url")
@@ -21,12 +21,8 @@ fn main() {
                 .takes_value(true)
                 .default_value("opc.tcp://localhost:4855")
                 .required(false))
-            .arg(Arg::with_name("subscribe")
-                .long("subscribe")
-                .help("Subscribes to values running indefinitely")
-                .required(false))
             .get_matches();
-        (m.is_present("subscribe"), m.value_of("url").unwrap().to_string())
+        m.value_of("url").unwrap().to_string()
     };
 
     // Optional - enable OPC UA logging
@@ -34,7 +30,6 @@ fn main() {
 
     // Use the sample client config to set up a client. The sample config has a number of named
     // endpoints one of which is marked as the default.
-
     let mut client = ClientBuilder::new()
         .application_name("Simple Client")
         .application_uri("urn:SimpleClient")
@@ -42,41 +37,13 @@ fn main() {
         .client().unwrap();
 
     if let Ok(session) = client.connect_to_endpoint((url.as_ref(), SecurityPolicy::None.to_str(), MessageSecurityMode::None, UserTokenPolicy::anonymous()), IdentityToken::Anonymous) {
-        // The --subscribe arg decides if code should subscribe to values, or just fetch those
-        // values and exit
-        let result = if subscribe {
-            subscription_loop(session)
-        } else {
-            read_values(session)
-        };
-        if let Err(result) = result {
+        if let Err(result) = subscription_loop(session) {
             println!("ERROR: Got an error while performing action - {}", result);
         }
     }
 }
 
-fn nodes_to_monitor() -> Vec<ReadValueId> {
-    vec![
-        ReadValueId::from(NodeId::new(2, "v1")),
-        ReadValueId::from(NodeId::new(2, "v2")),
-        ReadValueId::from(NodeId::new(2, "v3")),
-        ReadValueId::from(NodeId::new(2, "v4")),
-    ]
-}
-
-fn print_value(read_value_id: &ReadValueId, data_value: &DataValue) {
-    let node_id = read_value_id.node_id.to_string();
-    if let Some(ref value) = data_value.value {
-        println!("Item \"{}\", Value = {:?}", node_id, value);
-    } else {
-        println!("Item \"{}\", Value not found, error: {}", node_id, data_value.status.as_ref().unwrap());
-    }
-}
-
 fn subscription_loop(session: Arc<RwLock<Session>>) -> Result<(), StatusCode> {
-    // Create a subscription
-    println!("Creating subscription");
-
     // This scope is important - we don't want the session to be locked when the code hits the
     // loop below
     {
@@ -92,7 +59,12 @@ fn subscription_loop(session: Arc<RwLock<Session>>) -> Result<(), StatusCode> {
         println!("Created a subscription with id = {}", subscription_id);
 
         // Create some monitored items
-        let read_nodes = nodes_to_monitor();
+        let read_nodes = vec![
+            ReadValueId::from(NodeId::new(2, "v1")),
+            ReadValueId::from(NodeId::new(2, "v2")),
+            ReadValueId::from(NodeId::new(2, "v3")),
+            ReadValueId::from(NodeId::new(2, "v4")),
+        ];
         let items_to_create: Vec<MonitoredItemCreateRequest> = read_nodes.into_iter().map(|read_node| {
             MonitoredItemCreateRequest::new(read_node, MonitoringMode::Reporting, MonitoringParameters::default())
         }).collect();
@@ -105,20 +77,11 @@ fn subscription_loop(session: Arc<RwLock<Session>>) -> Result<(), StatusCode> {
     Ok(())
 }
 
-fn read_values(session: Arc<RwLock<Session>>) -> Result<(), StatusCode> {
-    // Fetch some values from the sample server
-    let read_nodes = nodes_to_monitor();
-    let data_values = {
-        let mut session = session.write().unwrap();
-        session.read_nodes(&read_nodes)?.unwrap()
-    };
-
-    // Print the values out
-    println!("Values from server:");
-    read_nodes.iter().zip(data_values.iter()).for_each(|i| {
-        print_value(i.0, i.1);
-    });
-
-    Ok(())
+fn print_value(read_value_id: &ReadValueId, data_value: &DataValue) {
+    let node_id = read_value_id.node_id.to_string();
+    if let Some(ref value) = data_value.value {
+        println!("Item \"{}\", Value = {:?}", node_id, value);
+    } else {
+        println!("Item \"{}\", Value not found, error: {}", node_id, data_value.status.as_ref().unwrap());
+    }
 }
-
