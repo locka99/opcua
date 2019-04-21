025fb8c8ff7596d4e11e39192e9d7a901d38a147
Adam Lock <locka99@gmail.com>
2019-04-21 14:31:01 +0100

    Add criterion benchmark to test speed of populating address space as it is now prior to improvement.

---
+++ server/benches/address_space.rs
@@ -0,0 +1,18 @@
+#[macro_use]
+extern crate criterion;
+
+use criterion::Criterion;
+use criterion::black_box;
+
+use opcua_server::address_space::AddressSpace;
+
+fn populate_address_space() {
+    let _address_space = AddressSpace::new();
+}
+
+fn address_space_benchmark(c: &mut Criterion) {
+    c.bench_function("address_space", |b| b.iter(|| populate_address_space()));
+}
+
+criterion_group!(benches, address_space_benchmark);
+criterion_main!(benches);
