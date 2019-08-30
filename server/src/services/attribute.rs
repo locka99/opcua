eeca2f622a545e772316496ef5e144ea7cf71710
Adam Lock <locka99@gmail.com>
2019-08-31 00:01:07 +0100

    Change comment

--- server/src/services/attribute.rs
+++ server/src/services/attribute.rs
@@ -21,9 +21,7 @@ impl AttributeService {
         AttributeService {}
     }
 
-    /// Spec:
-    ///
-    /// This Service is used to read historical values or Events of one or more Nodes. For
+    /// Used to read historical values or Events of one or more Nodes. For
     /// constructed Attribute values whose elements are indexed, such as an array, this Service
     /// allows Clients to read the entire set of indexed values as a composite, to read individual
     /// elements or to read ranges of elements of the composite. Servers may make historical
@@ -55,9 +53,7 @@ impl AttributeService {
         }
     }
 
-    /// Spec:
-    ///
-    /// This Service is used to write values to one or more Attributes of one or more Nodes. For
+    /// Used to write values to one or more Attributes of one or more Nodes. For
     /// constructed Attribute values whose elements are indexed, such as an array, this Service
     /// allows Clients to write the entire set of indexed values as a composite, to write individual
     /// elements or to write ranges of elements of the composite.
