3ce368beb2c6ff8d5a6a9df8e0f4fd07fdff8fc2
Adam Lock <locka99@gmail.com>
2019-10-13 19:23:33 +0100

    Update integration run scripts

--- integration/run-sanity.sh
+++ integration/run-sanity.sh
@@ -1,2 +1,3 @@
+#!/bin/sh
 export RUST_OPCUA_LOG=debug
 cargo test -- --exact --test-threads=1 --ignored tests::connect_none
--- integration/run.sh
+++ integration/run.sh
@@ -1,2 +1,3 @@
+#!/bin/sh
 export RUST_OPCUA_LOG=debug
-cargo test -- --test-threads=1 --ignored
+cargo test -- --test-threads=1 --ignored $1 $2 $3 $4
