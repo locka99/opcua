a8bac4a7ab5a76c77d77b2117de99368a837c16a
Adam Lock <locka99@gmail.com>
2019-12-19 21:27:19 +0000

    More historical work

--- samples/demo-server/src/historical.rs
+++ samples/demo-server/src/historical.rs
@@ -3,7 +3,12 @@
 use opcua_server::prelude::*;
 
 // Register some historical data providers
-pub fn add_providers(server: &mut Server) {}
+pub fn add_providers(server: &mut Server) {
+    let server_state = server.server_state();
+    let mut server_state = server_state.write().unwrap();
+    server_state.set_historical_data_provider(Box::new(DataProvider));
+    server_state.set_historical_event_provider(Box::new(EventProvider));
+}
 
 pub struct DataProvider;
 
--- server/src/historical/mod.rs
+++ server/src/historical/mod.rs
@@ -17,14 +17,17 @@ use crate::address_space::AddressSpace;
 /// if they are lengthy operations.
 pub trait HistoricalEventProvider {
     fn read_event_details(&self, _address_space: Arc<RwLock<AddressSpace>>, _request: ReadEventDetails, _timestamps_to_return: TimestampsToReturn, _release_continuation_points: bool, _nodes_to_read: &[HistoryReadValueId]) -> Result<Vec<HistoryReadResult>, StatusCode> {
+        info!("Unimplemented read_event_details");
         Err(StatusCode::BadHistoryOperationUnsupported)
     }
 
     fn update_event_details(&self, _address_space: Arc<RwLock<AddressSpace>>, _request: UpdateEventDetails) -> Result<Vec<StatusCode>, StatusCode> {
+        info!("Unimplemented update_event_details");
         Err(StatusCode::BadHistoryOperationUnsupported)
     }
 
     fn delete_event_details(&self, _address_space: Arc<RwLock<AddressSpace>>, _request: DeleteEventDetails) -> Result<Vec<StatusCode>, StatusCode> {
+        info!("Unimplemented delete_event_details");
         Err(StatusCode::BadHistoryOperationUnsupported)
     }
 }
@@ -45,30 +48,37 @@ pub trait HistoricalDataProvider {
     /// Note: Function returns an `HistoryRawData` enum containing *either* a `HistoryData` for a read raw action
     /// or a `HistoryModifiedData` for a read modified action.
     fn read_raw_modified_details(&self, _address_space: Arc<RwLock<AddressSpace>>, _request: ReadRawModifiedDetails, _timestamps_to_return: TimestampsToReturn, _release_continuation_points: bool, _nodes_to_read: &[HistoryReadValueId]) -> Result<Vec<HistoryReadResult>, StatusCode> {
+        info!("Unimplemented read_raw_modified_details");
         Err(StatusCode::BadHistoryOperationUnsupported)
     }
 
     fn read_processed_details(&self, _address_space: Arc<RwLock<AddressSpace>>, _request: ReadProcessedDetails, _timestamps_to_return: TimestampsToReturn, _release_continuation_points: bool, _nodes_to_read: &[HistoryReadValueId]) -> Result<Vec<HistoryReadResult>, StatusCode> {
+        info!("Unimplemented read_processed_details");
         Err(StatusCode::BadHistoryOperationUnsupported)
     }
 
     fn read_at_time_details(&self, _address_space: Arc<RwLock<AddressSpace>>, _request: ReadAtTimeDetails, _timestamps_to_return: TimestampsToReturn, _release_continuation_points: bool, _nodes_to_read: &[HistoryReadValueId]) -> Result<Vec<HistoryReadResult>, StatusCode> {
+        info!("Unimplemented read_at_time_details");
         Err(StatusCode::BadHistoryOperationUnsupported)
     }
 
     fn update_data_details(&self, _address_space: Arc<RwLock<AddressSpace>>, _request: UpdateDataDetails) -> Result<Vec<StatusCode>, StatusCode> {
+        info!("Unimplemented update_data_details");
         Err(StatusCode::BadHistoryOperationUnsupported)
     }
 
     fn update_structure_data_details(&self, _address_space: Arc<RwLock<AddressSpace>>, _request: UpdateStructureDataDetails) -> Result<Vec<StatusCode>, StatusCode> {
+        info!("Unimplemented update_structure_data_details");
         Err(StatusCode::BadHistoryOperationUnsupported)
     }
 
     fn delete_raw_modified_details(&self, _address_space: Arc<RwLock<AddressSpace>>, _request: DeleteRawModifiedDetails) -> Result<Vec<StatusCode>, StatusCode> {
+        info!("Unimplemented delete_raw_modified_details");
         Err(StatusCode::BadHistoryOperationUnsupported)
     }
 
     fn delete_at_time_details(&self, _address_space: Arc<RwLock<AddressSpace>>, _request: DeleteAtTimeDetails) -> Result<Vec<StatusCode>, StatusCode> {
+        info!("Unimplemented delete_at_time_details");
         Err(StatusCode::BadHistoryOperationUnsupported)
     }
 }
