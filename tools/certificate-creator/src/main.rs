2113834f6654377e31fe17f7466c077508574b27
Adam Lock <locka99@gmail.com>
2020-01-23 22:44:08 +0000

    Replace clap with pico-args in certificate-creator. Last one

--- tools/certificate-creator/src/main.rs
+++ tools/certificate-creator/src/main.rs
@@ -1,21 +1,18 @@
-use clap::value_t_or_exit;
-
 use std::path::PathBuf;
 
 use opcua_crypto::*;
 
 fn main() {
-    if let Ok((args, overwrite, path)) = parse_x509_args() {
+    if let Ok((x509_data, overwrite, path)) = parse_x509_args() {
         println!("Creating certificate...");
-
-        println!("  Key size = {}", args.key_size);
-        println!("  CN (common name) = \"{}\"", args.common_name);
-        println!("  O (organization) = \"{}\"", args.organization);
-        println!("  OU (organizational unit) = \"{}\"", args.organizational_unit);
-        println!("  C (country) = \"{}\"", args.country);
-        println!("  ST (state) = \"{}\"", args.state);
-        println!("  Duration = {} days", args.certificate_duration_days);
-        for i in args.alt_host_names.iter().enumerate() {
+        println!("  Key size = {}", x509_data.key_size);
+        println!("  CN (common name) = \"{}\"", x509_data.common_name);
+        println!("  O (organization) = \"{}\"", x509_data.organization);
+        println!("  OU (organizational unit) = \"{}\"", x509_data.organizational_unit);
+        println!("  C (country) = \"{}\"", x509_data.country);
+        println!("  ST (state) = \"{}\"", x509_data.state);
+        println!("  Duration = {} days", x509_data.certificate_duration_days);
+        for i in x509_data.alt_host_names.iter().enumerate() {
             if i.0 == 0 {
                 println!("  Application URI = \"{}\"", i.1);
             } else {
@@ -24,146 +21,132 @@ fn main() {
         }
 
         let cert_store = CertificateStore::new(&path);
-        if cert_store.create_and_store_application_instance_cert(&args, overwrite).is_err() {
+        if cert_store.create_and_store_application_instance_cert(&x509_data, overwrite).is_err() {
             eprintln!("Certificate creation failed, check above for errors");
         } else {
             println!("Certificate and private key have been written to {} and {}",
                      cert_store.own_cert_path().to_string_lossy(), cert_store.own_private_key_path().to_string_lossy());
         }
-    } else {
-        eprintln!("Invalid arguments, check above for errors");
     }
 }
 
-fn parse_x509_args() -> Result<(X509Data, bool, PathBuf), ()> {
-    use clap::*;
-    let matches = App::new("OPC UA Certificate Creator")
-        .author("Adam Lock <locka99@gmail.com>")
-        .about(
-            r#"OPC UA for Rust Certificate Creator.
+struct Args {
+    help: bool,
+    overwrite: bool,
+    key_size: u16,
+    pki_path: String,
+    duration: u32,
+    application_uri: String,
+    hostnames: String,
+    add_computer_name: bool,
+    add_localhost_name: bool,
+    common_name: String,
+    organization: String,
+    organizational_unit: String,
+    country: String,
+    state: String,
+}
 
-This will creates a self-signed key (private/private.pem) and X509 certificate (own/cert.der) for
+impl Args {
+    pub fn parse_args() -> Result<Args, Box<dyn std::error::Error>> {
+        let mut args = pico_args::Arguments::from_env();
+        Ok(Args {
+            help: args.contains(["-h", "--help"]),
+            overwrite: args.contains(["-o", "--overwrite"]),
+            key_size: args.opt_value_from_str("--key-size")?.unwrap_or(DEFAULT_KEY_SIZE),
+            pki_path: args.opt_value_from_str("--pkipath")?.unwrap_or(String::from(DEFAULT_PKI_PATH)),
+            duration: args.opt_value_from_str("--duration")?.unwrap_or(DEFAULT_DURATION),
+            application_uri: args.opt_value_from_str("--application-uri")?.unwrap_or(String::from(DEFAULT_APPLICATION_URI)),
+            hostnames: args.opt_value_from_str("--hostnames")?.unwrap_or(String::from("")),
+            add_computer_name: args.contains("--add-computer-name"),
+            add_localhost_name: args.contains("--add-localhost-name"),
+            common_name: args.opt_value_from_str("--CN")?.unwrap_or(String::from(DEFAULT_CN)),
+            organization: args.opt_value_from_str("--O")?.unwrap_or(String::from(DEFAULT_O)),
+            organizational_unit: args.opt_value_from_str("--OU")?.unwrap_or(String::from(DEFAULT_OU)),
+            country: args.opt_value_from_str("--C")?.unwrap_or(String::from(DEFAULT_C)),
+            state: args.opt_value_from_str("--ST")?.unwrap_or(String::from(DEFAULT_ST)),
+        })
+    }
+
+    pub fn usage() {
+        println!(r#"OPC UA Certificate Creator
+
+This creates a self-signed key (private/private.pem) and X509 certificate (own/cert.der) for
 use with OPC UA clients and servers. Use the flags to control what the certificate contains. For
 convenience some values will be prefilled from defaults, but for production purposes all defaults
 should be overridden.
 
-Files will be created under the specified under the specified --pkipath value."#)
-        .arg(Arg::with_name("keysize")
-            .long("keysize")
-            .help("Sets the key size(strength)")
-            .default_value("2048")
-            .takes_value(true)
-            .possible_values(&["2048", "4096"])
-            .required(false))
-        .arg(Arg::with_name("pkipath")
-            .long("pkipath")
-            .help("Path to the OPC UA for Rust pki/ directory")
-            .default_value(".")
-            .value_name("path")
-            .takes_value(true)
-            .required(false))
-        .arg(Arg::with_name("duration")
-            .long("duration")
-            .help("The duration in days of this certificate before it expires")
-            .value_name("days")
-            .default_value("365")
-            .takes_value(true)
-            .required(false))
-        .arg(Arg::with_name("overwrite")
-            .long("overwrite")
-            .help("Overwrites existing files"))
-        .arg(Arg::with_name("uri")
-            .long("application-uri")
-            .help("The application's uri used by OPC UA for authentication purposes.")
-            .default_value("urn:OPCUAForRust")
-            .takes_value(true))
-        .arg(Arg::with_name("hostnames")
-            .long("hostnames")
-            .help("Explicitly add the specified DNS names to the cert.")
-            .takes_value(true)
-            .value_names(&["dns1", "dns2"])
-            .multiple(true)
-            .required(false))
-        .arg(Arg::with_name("add-computer-name")
-            .long("add-computer-name")
-            .help("Add this computer's name (inferred from COMPUTERNAME / NAME environment variables) to the DNS names.")
-            .value_name("flag")
-            .takes_value(true)
-            .default_value("true"))
-        .arg(Arg::with_name("add-localhost-name")
-            .long("add-localhost-name")
-            .help("Add localhost, 127.0.0.1, ::1 to the DNS names.")
-            .value_name("flag")
-            .takes_value(true)
-            .default_value("false"))
-        .arg(Arg::with_name("CN")
-            .long("CN")
-            .help("Specifies the Common Name for the cert")
-            .default_value("OPC UA Demo Key")
-            .takes_value(true))
-        .arg(Arg::with_name("O")
-            .long("O")
-            .help("Specifies the Organization for the cert")
-            .default_value("OPC UA for Rust")
-            .takes_value(true))
-        .arg(Arg::with_name("OU")
-            .long("OU")
-            .help("Specifies the Organization Unit for the cert")
-            .default_value("Certificate Creator")
-            .takes_value(true))
-        .arg(Arg::with_name("C")
-            .long("C")
-            .help("Specifies the Country for the cert")
-            .default_value("IE")
-            .takes_value(true))
-        .arg(Arg::with_name("ST")
-            .long("ST")
-            .help("Specifies the State for the cert")
-            .default_value("Dublin")
-            .takes_value(true))
-        .get_matches();
-
-    let pki_path = matches.value_of("pkipath").unwrap().to_string();
-    let key_size = value_t_or_exit!(matches, "keysize", u32);
-    let overwrite = matches.is_present("overwrite");
-    let certificate_duration_days = value_t_or_exit!(matches, "duration", u32);
-
-    let common_name = matches.value_of("CN").unwrap().to_string();
-    let organization = matches.value_of("O").unwrap().to_string();
-    let organizational_unit = matches.value_of("OU").unwrap().to_string();
-    let country = matches.value_of("C").unwrap().to_string();
-    let state = matches.value_of("ST").unwrap().to_string();
-    let application_uri = matches.value_of("uri").unwrap().to_string();
-    let add_localhost = value_t_or_exit!(matches, "add-localhost-name", bool);
-    let add_computer_name = value_t_or_exit!(matches, "add-computer-name", bool);
-
-    if certificate_duration_days == 0 {
-        eprintln!("Duration is zero days!?");
-        return Err(());
+Usage:
+  -h, --help            Show help.
+  -o, --overwrite       Overwrites existing files.
+  --key-size size       Sets the key size in bits - [2048, 4096] (default: {})
+  --pkipath path        Path to the OPC UA for Rust pki/ directory. (default: {})
+  --duration days       The duration in days of this certificate before it expires. (default: {})
+  --application-uri     The application's uri used by OPC UA for authentication purposes. (default: {})
+  --add-computer-name   Add this computer's name (inferred from COMPUTERNAME / NAME environment variables) to the alt host names.
+  --add-localhost-name  Add localhost, 127.0.0.1, ::1 to the alt host names.
+  --hostnames names     Comma separated list of DNS/IP names to add as subject alt host names.
+  --CN name             Specifies the Common Name for the cert (default: {}).
+  --O name              Specifies the Organization for the cert (default: {}).
+  --OU name             Specifies the Organization Unit for the cert (default: {}).
+  --C name              Specifies the Country for the cert (default: {}).
+  --ST name             "Specifies the State for the cert. (default: {})"#,
+                 DEFAULT_KEY_SIZE, DEFAULT_PKI_PATH, DEFAULT_DURATION, DEFAULT_APPLICATION_URI, DEFAULT_CN, DEFAULT_O, DEFAULT_OU, DEFAULT_C, DEFAULT_ST);
     }
+}
 
-    // Create alt host names for application uri, localhost and computer name if required
-    let mut alt_host_names = X509Data::alt_host_names(&application_uri, add_localhost, add_computer_name);
+const DEFAULT_KEY_SIZE: u16 = 2048;
+const DEFAULT_PKI_PATH: &'static str = ".";
+const DEFAULT_DURATION: u32 = 365;
+const DEFAULT_APPLICATION_URI: &'static str = "urn:OPCUAForRust";
+const DEFAULT_CN: &'static str = "OPC UA Demo Key";
+const DEFAULT_O: &'static str = "OPC UA for Rust";
+const DEFAULT_OU: &'static str = "Certificate Creator";
+const DEFAULT_C: &'static str = "IE";
+const DEFAULT_ST: &'static str = "Dublin";
 
-    // Add the host names that were supplied by argument
-    if let Some(hostnames) = matches.values_of("hostnames") {
-        for h in hostnames {
-            alt_host_names.push(h.to_string());
+fn parse_x509_args() -> Result<(X509Data, bool, PathBuf), ()> {
+    // Read command line arguments
+    let args = Args::parse_args()
+        .map_err(|_| Args::usage())?;
+    if args.help || ![2048u16, 4096u16].contains(&args.key_size) || args.duration == 0 {
+        Args::usage();
+        Err(())
+    } else {
+        let pki_path = args.pki_path;
+        let key_size = args.key_size as u32;
+        let overwrite = args.overwrite;
+        let certificate_duration_days = args.duration;
+
+        let common_name = args.common_name;
+        let organization = args.organization;
+        let organizational_unit = args.organizational_unit;
+        let country = args.country;
+        let state = args.state;
+        let application_uri = args.application_uri;
+        let add_localhost = args.add_localhost_name;
+        let add_computer_name = args.add_computer_name;
+
+        // Create alt host names for application uri, localhost and computer name if required
+        let mut alt_host_names = X509Data::alt_host_names(&application_uri, add_localhost, add_computer_name);
+
+        // Add the host names that were supplied by argument
+        let mut hostnames: Vec<String> = args.hostnames.split(",").map(|s| s.to_string()).collect();
+        alt_host_names.append(&mut hostnames);
+        if alt_host_names.len() == 1 {
+            eprintln!("No alt host names were supplied or could be inferred. Certificate is useless without at least one DNS entry.");
+            return Err(());
         }
-    }
-    if alt_host_names.len() == 1 {
-        eprintln!("No alt host names were supplied or could be inferred. Certificate is useless without at least one DNS entry.");
-        return Err(());
-    }
 
-    Ok((X509Data {
-        key_size,
-        common_name,
-        organization,
-        organizational_unit,
-        country,
-        state,
-        alt_host_names,
-        certificate_duration_days,
-    }, overwrite, PathBuf::from(&pki_path)))
+        Ok((X509Data {
+            key_size,
+            common_name,
+            organization,
+            organizational_unit,
+            country,
+            state,
+            alt_host_names,
+            certificate_duration_days,
+        }, overwrite, PathBuf::from(&pki_path)))
+    }
 }
