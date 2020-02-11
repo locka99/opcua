708c8c0d932c0f785e2ca23ff7b1f8c2587514f5
Adam Lock <locka99@gmail.com>
2020-02-11 20:32:38 +0000

    More audit work

--- server/src/events/audit.rs
+++ server/src/events/audit.rs
@@ -1,11 +1,14 @@
+use std::sync::{Arc, RwLock};
+
 use opcua_types::*;
 
 use crate::{
     address_space::address_space::AddressSpace,
     events::event::{BaseEventType, Event},
+    session::Session,
 };
 
-pub(crate) struct AuditLog {}
+trait AuditEvent: Event {}
 
 /// Base type for audit events. Do not raise events of this type
 struct AuditEventType {
@@ -16,6 +19,8 @@ struct AuditEventType {
     client_audit_entry_id: UAString,
     client_user_id: UAString,
 }
+
+impl AuditEvent for AuditEventType {}
 
 impl Event for AuditEventType {
     type Err = ();
@@ -47,6 +52,8 @@ struct AuditSecurityEventType {
     base: AuditEventType
 }
 
+impl AuditEvent for AuditSecurityEventType {}
+
 impl Event for AuditSecurityEventType {
     type Err = ();
 
@@ -64,6 +71,8 @@ struct AuditSessionEventType {
     base: AuditSecurityEventType,
     session_id: NodeId,
 }
+
+impl AuditEvent for AuditSessionEventType {}
 
 impl Event for AuditSessionEventType {
     type Err = ();
@@ -88,6 +97,8 @@ pub struct AuditCreateSessionEventType {
     revised_session_timeout: Duration,
 }
 
+impl AuditEvent for AuditCreateSessionEventType {}
+
 impl Event for AuditCreateSessionEventType {
     type Err = ();
 
@@ -108,10 +119,12 @@ impl Event for AuditCreateSessionEventType {
 
 pub struct AuditActivateSessionEventType {
     base: AuditSessionEventType,
-    client_software_certificates: SignedSoftwareCertificate,
-    // pub user_identity_token: References,
+    client_software_certificates: Vec<SignedSoftwareCertificate>,
+    user_identity_token: UserIdentityToken,
     secure_channel_id: UAString,
 }
+
+impl AuditEvent for AuditActivateSessionEventType {}
 
 impl Event for AuditActivateSessionEventType {
     type Err = ();
@@ -123,8 +136,17 @@ impl Event for AuditActivateSessionEventType {
     fn raise(self, address_space: &mut AddressSpace) -> Result<NodeId, Self::Err> {
         let node_id = self.base.raise(address_space)?;
         let ns = node_id.namespace;
-        // TODO client software certificates (extension object i=344)
-        // TODO user identity token (extension object i=316)
+        // Client software certificates is an array of extension objects (extension object i=344)
+        let client_software_certificates =
+            self.client_software_certificates.iter().map(|c| {
+                Variant::from(ExtensionObject::from_encodable(ObjectId::SignedSoftwareCertificate_Encoding_DefaultBinary, c))
+            }).collect::<Vec<_>>();
+        Self::add_property(&node_id, NodeId::next_numeric(ns), "ClientSoftwareCertificates", "ClientSoftwareCertificates", client_software_certificates, address_space);
+
+        // User identity token (extension object i=316)
+        let user_identity_token = ExtensionObject::from_encodable(ObjectId::UserIdentityToken_Encoding_DefaultBinary, &self.user_identity_token);
+        Self::add_property(&node_id, NodeId::next_numeric(ns), "UserIdentityToken", "UserIdentityToken", user_identity_token, address_space);
+
         Self::add_property(&node_id, NodeId::next_numeric(ns), "SecureChannelId", "SecureChannelId", self.secure_channel_id.clone(), address_space);
         Ok(node_id)
     }
@@ -134,6 +156,8 @@ pub struct AuditCancelEventType {
     base: AuditSessionEventType,
     request_handle: u32,
 }
+
+impl AuditEvent for AuditCancelEventType {}
 
 impl Event for AuditCancelEventType {
     type Err = ();
@@ -155,6 +179,8 @@ pub struct AuditCertificateEventType {
     certificate: ByteString,
 }
 
+impl AuditEvent for AuditCertificateEventType {}
+
 impl Event for AuditCertificateEventType {
     type Err = ();
 
@@ -174,6 +200,8 @@ pub struct AuditCertificateDataMismatchEventType {
     base: AuditCertificateEventType
 }
 
+impl AuditEvent for AuditCertificateDataMismatchEventType {}
+
 impl Event for AuditCertificateDataMismatchEventType {
     type Err = ();
 
@@ -189,6 +217,8 @@ impl Event for AuditCertificateDataMismatchEventType {
 pub struct AuditCertificateExpiredEventType {
     base: AuditCertificateEventType
 }
+
+impl AuditEvent for AuditCertificateExpiredEventType {}
 
 impl Event for AuditCertificateExpiredEventType {
     type Err = ();
@@ -206,6 +236,8 @@ pub struct AuditCertificateInvalidEventType {
     base: AuditCertificateEventType
 }
 
+impl AuditEvent for AuditCertificateInvalidEventType {}
+
 impl Event for AuditCertificateInvalidEventType {
     type Err = ();
 
@@ -221,6 +253,8 @@ impl Event for AuditCertificateInvalidEventType {
 pub struct AuditCertificateUntrustedEventType {
     base: AuditCertificateEventType
 }
+
+impl AuditEvent for AuditCertificateUntrustedEventType {}
 
 impl Event for AuditCertificateUntrustedEventType {
     type Err = ();
@@ -238,6 +272,8 @@ pub struct AuditCertificateRevokedEventType {
     pub base: AuditCertificateEventType
 }
 
+impl AuditEvent for AuditCertificateRevokedEventType {}
+
 impl Event for AuditCertificateRevokedEventType {
     type Err = ();
 
@@ -253,6 +289,8 @@ impl Event for AuditCertificateRevokedEventType {
 pub struct AuditCertificateMismatchEventType {
     base: AuditCertificateEventType
 }
+
+impl AuditEvent for AuditCertificateMismatchEventType {}
 
 impl Event for AuditCertificateMismatchEventType {
     type Err = ();
@@ -270,6 +308,8 @@ pub struct AuditNodeManagementEventType {
     base: AuditEventType
 }
 
+impl AuditEvent for AuditNodeManagementEventType {}
+
 impl Event for AuditNodeManagementEventType {
     type Err = ();
 
@@ -282,4 +322,43 @@ impl Event for AuditNodeManagementEventType {
     }
 }
 
-impl AuditLog {}
+pub(crate) struct AuditLog {
+    address_space: Arc<RwLock<AddressSpace>>,
+}
+
+impl AuditLog {
+    pub fn new(address_space: Arc<RwLock<AddressSpace>>) -> AuditLog {
+        AuditLog {
+            address_space
+        }
+    }
+
+    fn log<T>(&self, event: T) -> Result<NodeId, ()> where T: AuditEvent + Event {
+        let mut address_space = trace_write_lock_unwrap!(self.address_space);
+        event.raise(&mut address_space).map_err(|_| ())
+    }
+
+    pub fn log_open_secure_channel(&self, session: Arc<RwLock<Session>>) {}
+
+    pub fn log_cancel(&self, session: Arc<RwLock<Session>>, audit_entry_id: &str, client_entry_id: &UAString, request_handle: u32) {}
+    pub fn log_update_method(&self, session: Arc<RwLock<Session>>) {}
+
+    pub fn log_activate_session(&self) {}
+    pub fn log_create_session(&self) {}
+    pub fn log_url_mismatch(&self) {}
+
+    pub fn log_certificate_data_mismatch(&self) {}
+    pub fn log_certificate_expired(&self) {}
+    pub fn log_certificate_invalid(&self) {}
+    pub fn log_certificate_untrusted(&self) {}
+    pub fn log_certificate_revoked(&self) {}
+    pub fn log_certificate_mismatch(&self) {}
+
+    pub fn log_add_nodes(&self) {}
+    pub fn log_delete_nodes(&self) {}
+    pub fn log_add_references(&self) {}
+    pub fn log_delete_references(&self) {}
+
+    pub fn log_write_update(&self) {}
+    pub fn log_history_update(&self) {}
+}
