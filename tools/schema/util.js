bf1eb7b6d90aac4b41072941eee88ad4f7ae969c
Adam Lock <locka99@gmail.com>
2020-02-24 09:20:20 +0000

    Add some code to cope with namespace qualified node ids

--- tools/schema/util.js
+++ tools/schema/util.js
@@ -6,3 +6,18 @@ exports.write_to_file = function (file_path, contents) {
     fs.writeSync(fd, buffer, 0, buffer.length, null);
     fs.closeSync(fd);
 };
+
+// Parses a string node id into an object with
+exports.parse_node_id = function (node_id) {
+    let re = /(?:ns=([0-9]+);)?(i|b|s|g)=([a-zA-Z0-9\-]+)/;
+    let m = node_id.toString().match(re);
+    if (m) {
+        return {
+            ns: m[1] ? m[1] : 0,
+            type: m[2],
+            value: m[3],
+        };
+    } else {
+        return null;
+    }
+}
