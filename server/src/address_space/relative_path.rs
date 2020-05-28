e26a65c649111fcc459f4ca225b94cb3092de47b
Adam Lock <locka99@gmail.com>
2020-05-28 19:56:22 +0100

    Add debug diagnostics to call

--- server/src/address_space/relative_path.rs
+++ server/src/address_space/relative_path.rs
@@ -47,6 +47,7 @@ pub(crate) fn find_node_from_browse_path<'a>(address_space: &'a AddressSpace, pa
                 if let Some(found_node_id) = found_node_id {
                     parent_node_id = found_node_id.clone();
                 } else {
+                    debug!("Cannot find node under {:?} with browse_path of {:?}", parent_node_id, browse_path);
                     return Err(StatusCode::BadNotFound);
                 }
             } else {
