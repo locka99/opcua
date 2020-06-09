235f7151534b3b1eb3cf33ce8a45ec31e18b53b8
Adam Lock <locka99@gmail.com>
2020-06-09 20:47:36 +0100

    Add a new method that takes an input parameter

--- samples/demo-server/src/methods.rs
+++ samples/demo-server/src/methods.rs
@@ -21,6 +21,7 @@ pub fn add_methods(server: &mut Server, ns: u16) {
         .organized_by(ObjectId::ObjectsFolder)
         .insert(&mut address_space);
 
+    // HelloWorld takes zero args and returns "Hello World" in a result parameter
     let fn_node_id = NodeId::new(ns, "HelloWorld");
     MethodBuilder::new(&fn_node_id, "HelloWorld", "HelloWorld")
         .component_of(object_id.clone())
@@ -29,17 +30,63 @@ pub fn add_methods(server: &mut Server, ns: u16) {
         ])
         .callback(Box::new(HelloWorld))
         .insert(&mut address_space);
+
+    // HelloX takes one arg and returns "Hello World" in a result parameter
+    let fn_node_id = NodeId::new(ns, "HelloX");
+    MethodBuilder::new(&fn_node_id, "HelloX", "HelloX")
+        .component_of(object_id.clone())
+        .input_args(&mut address_space, &[
+            ("YourName", DataTypeId::String).into()
+        ])
+        .output_args(&mut address_space, &[
+            ("Result", DataTypeId::String).into()
+        ])
+        .callback(Box::new(HelloX))
+        .insert(&mut address_space);
 }
 
 pub struct HelloWorld;
 
 impl callbacks::Method for HelloWorld {
     fn call(&mut self, _session: &mut Session, _request: &CallMethodRequest) -> Result<CallMethodResult, StatusCode> {
+        let message = format!("Hello World!");
         Ok(CallMethodResult {
             status_code: StatusCode::Good,
-            input_argument_results: Some(vec![StatusCode::Good]),
+            input_argument_results: None,
             input_argument_diagnostic_infos: None,
-            output_arguments: Some(vec![Variant::from("Hello World!")]),
+            output_arguments: Some(vec![Variant::from(message)]),
+        })
+    }
+}
+
+pub struct HelloX;
+
+impl callbacks::Method for HelloX {
+    fn call(&mut self, _session: &mut Session, request: &CallMethodRequest) -> Result<CallMethodResult, StatusCode> {
+        // Validate input to be a string
+        let mut out1 = Variant::Empty;
+        let in1_result = if let Some(ref input_arguments) = request.input_arguments {
+            if let Some(in1) = input_arguments.get(0) {
+                if let Variant::String(in1) = in1 {
+                    out1 = Variant::from(format!("Hello {}!", &in1));
+                    StatusCode::Good
+                } else {
+                    StatusCode::BadInvalidArgument
+                }
+            } else if input_arguments.len() == 0 {
+                StatusCode::BadArgumentsMissing
+            } else {
+                // Shouldn't get here because there is 1 argument
+                StatusCode::BadTooManyArguments
+            }
+        } else {
+            StatusCode::BadArgumentsMissing
+        };
+        Ok(CallMethodResult {
+            status_code: StatusCode::Good,
+            input_argument_results: Some(vec![in1_result]),
+            input_argument_diagnostic_infos: None,
+            output_arguments: Some(vec![out1]),
         })
     }
 }
