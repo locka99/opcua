ade1fc73e3955561431f7dec1a54a8b343d007e7
Adam Lock <locka99@gmail.com>
2021-05-03 13:33:43 +0100

    Fix BadTimestampsToReturnInvalid not being returned from read service by using Invalid enum to bypass chunk decoding logic and allow service to process request.

--- types/src/service_types/enums.rs
+++ types/src/service_types/enums.rs
@@ -1404,10 +1404,9 @@ impl BinaryEncoder<TimestampsToReturn> for TimestampsToReturn {
             1 => Ok(Self::Server),
             2 => Ok(Self::Both),
             3 => Ok(Self::Neither),
-            4 => Ok(Self::Invalid),
             v => {
                 error!("Invalid value {} for enum TimestampsToReturn", v);
-                Err(StatusCode::BadUnexpectedError)
+                Ok(Self::Invalid)
             }
         }
     }
