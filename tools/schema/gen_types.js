d3621a1922a231b6df20c12c120039b711ca800b
Adam Lock <locka99@gmail.com>
2022-02-03 15:23:16 +0000

    Cleanup type generation, reinstate whitespaces. Fix filename in autogenerated comment to be correct

--- tools/schema/gen_types.js
+++ tools/schema/gen_types.js
@@ -2,5 +2,4 @@ let settings = require("./settings");
 let types_xml = `${settings.schema_dir}/Opc.Ua.Types.bsd`;
 let types = require("./types");
 
-
 types.from_xml(types_xml, settings.rs_types_dir);
