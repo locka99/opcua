ccc41587ef33531db343e5a055e1bee9961f4c55
Adam Lock <locka99@gmail.com>
2022-08-28 11:31:20 +0100

    Make console logging look a little prettier

--- lib/src/console_logging/mod.rs
+++ lib/src/console_logging/mod.rs
@@ -2,10 +2,24 @@
 // SPDX-License-Identifier: MPL-2.0
 // Copyright (C) 2017-2022 Adam Lock
 
-use std::io::Write;
-use std::sync::atomic::{AtomicBool, Ordering};
+use std::{
+    fmt,
+    io::Write,
+    sync::atomic::{AtomicBool, Ordering},
+};
 
-use env_logger::{Builder, Color};
+use env_logger::{fmt::Color, Builder};
+
+struct Pad<T> {
+    value: T,
+    width: usize,
+}
+
+impl<T: fmt::Display> fmt::Display for Pad<T> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{: <width$}", self.value, width = self.width)
+    }
+}
 
 pub fn init() {
     lazy_static! {
@@ -18,12 +32,11 @@ pub fn init() {
         // env_logger/RUST_LOG is used by cargo and other rust tools so console fills with garbage from
         // other processes  when we're only interested in our own garbage!
         let mut builder = Builder::from_env("RUST_OPCUA_LOG");
-        builder.format(|buf, record| {
+        builder.format(|f, record| {
             let now = chrono::Utc::now();
             let time_fmt = now.format("%Y-%m-%d %H:%M:%S%.3f");
 
-            let mut style = buf.style();
-
+            let mut style = f.style();
             match record.metadata().level() {
                 log::Level::Error => {
                     // White on red
@@ -38,17 +51,30 @@ pub fn init() {
                     // Blue on black
                     style.set_color(Color::Cyan);
                 }
+                log::Level::Debug => {
+                    // Blue
+                    style.set_color(Color::Green);
+                }
+                log::Level::Trace => {
+                    // Grey
+                    style.set_color(Color::Ansi256(8));
+                }
                 _ => {}
             }
+            let level = style.value(Pad {
+                value: record.level(),
+                width: 5,
+            });
 
-            writeln!(
-                buf,
-                "{} - {} - {} - {}",
-                time_fmt,
-                style.value(record.level()),
-                record.target(),
-                record.args()
-            )
+            let mut style = f.style();
+            let target = style.set_bold(true).value(Pad {
+                value: record.target(),
+                width: 40,
+            });
+
+            let args = record.args();
+
+            writeln!(f, "{} {} {} {}", time_fmt, level, target, args)
         });
         builder.init();
         info!("Logging is enabled, use RUST_OPCUA_LOG environment variable to control filtering, logging level");
