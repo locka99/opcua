21bfb6ce939671e4c35d90ef18806531fc4f102b
Adam Lock <locka99@gmail.com>
2022-10-05 14:51:28 +0100

    Add sub for sample

---
+++ samples/pubsub-server/src/main.rs
@@ -0,0 +1,63 @@
+
+use std::error::Error;
+use std::net::SocketAddr;
+use std::{env, io};
+use tokio::net::UdpSocket;
+
+struct Server {
+    socket: UdpSocket,
+    buf: Vec<u8>,
+    to_send: Option<(usize, SocketAddr)>,
+}
+
+impl Server {
+    async fn run(self) -> Result<(), io::Error> {
+        let Server {
+            socket,
+            mut buf,
+            mut to_send,
+        } = self;
+
+        loop {
+            // TODO replace with pubsub
+
+            // ----> REMOVE
+
+            // First we check to see if there's a message we need to echo back.
+            // If so then we try to send it back to the original source, waiting
+            // until it's writable and we're able to do so.
+            if let Some((size, peer)) = to_send {
+                let amt = socket.send_to(&buf[..size], &peer).await?;
+
+                println!("Echoed {}/{} bytes to {}", amt, size, peer);
+            }
+
+            // If we're here then `to_send` is `None`, so we take a look for the
+            // next message we're going to echo back.
+            to_send = Some(socket.recv_from(&mut buf).await?);
+
+            // <---- REMOVE
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let addr = env::args()
+        .nth(1)
+        .unwrap_or_else(|| "127.0.0.1:8080".to_string());
+
+    let socket = UdpSocket::bind(&addr).await?;
+    println!("Listening on: {}", socket.local_addr()?);
+
+    let server = Server {
+        socket,
+        buf: vec![0; 1024],
+        to_send: None,
+    };
+
+    // This starts the server task.
+    server.run().await?;
+
+    Ok(())
+}
