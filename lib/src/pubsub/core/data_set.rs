b44482ed01e3d37adccd8251d7541d11e354b57c
Adam Lock <locka99@gmail.com>
2023-03-23 09:02:35 +0000

    Work in progress

--- lib/src/pubsub/core/data_set.rs
+++ lib/src/pubsub/core/data_set.rs
@@ -2,12 +2,41 @@ use crate::types::*;
 
 use super::*;
 
-pub struct DataSetMessageField {
-    value: DataValue
+pub struct DataSetField {
+    /// Alias for the variable
+    alias: String,
+    /// Value
+    value: DataValue,
+    /// Flag inidicating if the field is promoted to the DataSetMessageHeader
+    promoted: bool
+}
+
+impl DataSetField {
+
 }
 
 pub struct DataSet {
-    pub flags: DataSetFieldFlags,
-    pub meta_data: DataSetMetaData,
-    pub fields: Vec<DataSetMessageField>
+    meta_data: DataSetMetaData,
+    values: Vec<DataSetField>
+}
+
+impl DataSet {
+    pub fn new(meta_data: DataSetMetaData) -> DataSet {
+        Self {
+            meta_data,
+            values: Vec::new()
+        }
+    }
+
+    pub fn add_variables(&mut self, values: &[DataSetField]) {
+        self.values.push(values);
+    }
+
+    pub fn meta_data(&self) -> &DataSetMetaData {
+        &self.meta_data
+    }
+
+    pub fn values(&self) -> &Vec<DataSetField> {
+        &self.values
+    }
 }
--- lib/src/pubsub/core/published_data_set.rs
+++ lib/src/pubsub/core/published_data_set.rs
@@ -1,5 +1,7 @@
 use std::sync::Arc;
 
+use crate::client::prelude::StatusCode;
+
 use super::DataSetWriter;
 
 pub struct PublishedDataSet {
@@ -16,7 +18,61 @@ impl Default for PublishedDataSet {
 }
 
 impl PublishedDataSet {
-    pub fn add(&mut self, writer: Arc<Box<dyn DataSetWriter>>) {
+    pub fn add_writer(&mut self, writer: Arc<Box<dyn DataSetWriter>>) {
         self.writers.push(writer);
     }
+}
+
+const MAX_PUBLISHED_VARIABLES: usize = 10;
+
+struct PublishedDataItems {
+    last_configuration_version: ConfigurationVersion,
+    // TODO ConfigurationVersionDataType
+
+    // TODO PublishedDataSetDataType
+
+    // TODO PublishedDataSetSourceDataType
+    variables: Vec<(String, bool, PublishedVariableDataType)>,
+}
+
+impl PublishedDataItems {
+    /// This function is internally equivalent to AddVariables in the address space so takes
+    /// similar arguments and yields similar responses.
+    pub fn add_variables(
+        &mut self,
+        configuration_version: ConfigurationVersion,
+        aliases: &[&str],
+        promoted_fields: &[bool],
+        variables: &[PublishedVariableDataType],
+    ) -> Result<(ConfigurationVersion, Vec<StatusCode>), StatusCode> {
+        if configuration_version != self.last_configuration_version {
+            Err(StatusCode::BadInvalidState)
+        } else if aliases.len() != promoted_fields.len() || aliases.len() != variables.len() {
+            Err(StatusCode::BadInvalidArgument)
+        } else if aliases.is_empty() {
+            Err(StatusCode::BadNothingToDo)
+        } else {
+            self.last_configuration_version = configuration_version;
+            let result= aliases
+                .iter()
+                .zip(promoted_fields.iter())
+                .zip(variables.iter())
+                .map(|v| {
+                if self.variables.len() >= MAX_PUBLISHED_VARIABLES {
+                    StatusCode::BadUnexpectedError // BadTooManyVariables
+                } else {
+                    // TODO NodeIdInvalid
+                    // TODO NodeIdUnknown
+                    // TODO IndexRangeInvalid
+                    // TODO IndexRangeNoData
+                    self.variables.push(v);
+                    StatusCode::Good
+                }
+            }).collect();
+            Ok((self.last_configuration_version.clone(), results))
+        }
+    }
+
+    // TODO remove variables
+    pub fn remove_variables(&mut self) {}
 }
