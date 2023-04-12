2af801d07fe6fd35c347df4b4aa18426224d2527
Adam Lock <locka99@gmail.com>
2023-04-12 18:31:26 +0100

    Fix compile error

--- lib/src/server/subscriptions/subscriptions.rs
+++ lib/src/server/subscriptions/subscriptions.rs
@@ -321,7 +321,9 @@ impl Subscriptions {
                 publish_request_timeout as u64
             });
             // The request has timed out if the timestamp plus hint exceeds the input time
-            if now.signed_duration_since(request_timestamp) > publish_request_timeout {
+            // TODO unwrap logic needs to change
+            let signed_duration_since: Duration = now.signed_duration_since(request_timestamp).to_std().unwrap();
+            if signed_duration_since > publish_request_timeout {
                 debug!("Publish request {} has expired - timestamp = {:?}, expiration hint = {}, publish timeout = {:?}, time now = {:?}, ", request_header.request_handle, request_timestamp, request_timestamp, publish_request_timeout, now);
                 expired_publish_responses.push_front(PublishResponseEntry {
                     request_id: request.request_id,
