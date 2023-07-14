56d25dbb358a2df79534db874061ffcbdf1d1bbd
Sidi Ali Mebarek Abdelhak <abdelhak@rmbtech.tech>
2023-07-14 11:24:47 +0100

    Run session loop in seperate tokio runtime

--- lib/src/client/session/session.rs
+++ lib/src/client/session/session.rs
@@ -681,12 +681,16 @@ impl Session {
             }
         };
         // Spawn the task on the alloted runtime
-        let runtime = {
-            let session = trace_read_lock!(session);
-            session.runtime.clone()
-        };
-        let runtime = trace_lock!(runtime);
-        runtime.block_on(task);
+        // let runtime = {
+        //     let session = trace_read_lock!(session);
+        //     session.runtime.clone()
+        // };
+        // let runtime = trace_lock!(runtime);
+        // runtime.block_on(task);
+
+        // The above code didn't release the lock and allow the subscriptions to respawn on reconnect.
+        // Creating a fresh runtime to run the session task as it's independent.
+        tokio::runtime::Runtime::new().unwrap().block_on(task);
     }
 
     /// Polls on the session which basically dispatches any pending
