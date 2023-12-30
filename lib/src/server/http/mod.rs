880ba3e7b4800364413de8ef76af57926c5b0660
Adam Lock <locka99@gmail.com>
2023-12-30 14:46:18 +0000

    Upgrade to latest Actix Web and also fix some broken logic in demo server

--- lib/src/server/http/mod.rs
+++ lib/src/server/http/mod.rs
@@ -2,14 +2,12 @@
 // SPDX-License-Identifier: MPL-2.0
 // Copyright (C) 2017-2022 Adam Lock
 
-use std::{
-    path::PathBuf,
-    sync::{mpsc, Arc},
-    thread,
-};
+use std::{path::PathBuf, sync::Arc, thread};
 
-use actix_web::{actix, fs, http, server, App, HttpRequest, HttpResponse, Responder};
-use tokio::time::{interval_at, Duration, Instant};
+use crate::core::runtime;
+use actix_files as fs;
+use actix_web::{web, App, HttpResponse, HttpServer, Responder, Result};
+use tokio::runtime::Runtime;
 
 use crate::sync::*;
 
@@ -17,17 +15,25 @@ use crate::server::{metrics::ServerMetrics, server::Connections, state::ServerSt
 
 /// This is our metrics service, the thing called to handle requests coming from hyper
 #[derive(Clone)]
-struct HttpState {
+struct AppState {
     server_state: Arc<RwLock<ServerState>>,
     connections: Arc<RwLock<Connections>>,
     server_metrics: Arc<RwLock<ServerMetrics>>,
+    base_path: Arc<RwLock<PathBuf>>,
 }
 
-fn abort(req: &HttpRequest<HttpState>) -> impl Responder {
+async fn index(data: web::Data<AppState>) -> Result<fs::NamedFile> {
+    let base_path = data.base_path.read();
+    let mut index_path = base_path.clone();
+    index_path.push("index.html");
+    debug!("Resolving index.html to location {}", index_path.display());
+    Ok(fs::NamedFile::open(index_path)?)
+}
+
+async fn abort(data: web::Data<AppState>) -> impl Responder {
     if cfg!(debug_assertions) {
-        let state = req.state();
         // Abort the server from the command
-        let mut server_state = state.server_state.write();
+        let mut server_state = data.server_state.write();
         server_state.abort();
         HttpResponse::Ok().content_type("text/plain").body("OK")
     } else {
@@ -38,27 +44,25 @@ fn abort(req: &HttpRequest<HttpState>) -> impl Responder {
     }
 }
 
-fn metrics(req: &HttpRequest<HttpState>) -> impl Responder {
+async fn metrics(data: web::Data<AppState>) -> impl Responder {
     use std::ops::Deref;
-
-    let state = req.state();
 
     // Send metrics data as json
     let json = {
         // Careful with the ordering here to avoid potential deadlock. Metrics are locked
         // several times in scope to avoid deadlocks issues.
         {
-            let server_state = state.server_state.read();
-            let mut server_metrics = state.server_metrics.write();
+            let server_state = data.server_state.read();
+            let mut server_metrics = data.server_metrics.write();
             server_metrics.update_from_server_state(&server_state);
         }
 
         // Take a copy of connections
         let connections = {
-            let connections = state.connections.read();
+            let connections = data.connections.read();
             connections.clone()
         };
-        let mut server_metrics = state.server_metrics.write();
+        let mut server_metrics = data.server_metrics.write();
         server_metrics.update_from_connections(connections);
         serde_json::to_string_pretty(server_metrics.deref()).unwrap()
     };
@@ -70,6 +74,7 @@ fn metrics(req: &HttpRequest<HttpState>) -> impl Responder {
 
 /// Runs an http server on the specified binding address, serving out the supplied server metrics
 pub fn run_http_server(
+    runtime: &Runtime,
     address: &str,
     content_path: &str,
     server_state: Arc<RwLock<ServerState>>,
@@ -78,66 +83,41 @@ pub fn run_http_server(
 ) {
     let address = String::from(address);
     let base_path = PathBuf::from(content_path);
-
-    let (tx, rx) = mpsc::channel();
-
     let server_state_http = server_state.clone();
+
+    // Getting this working was very painful since Actix HttpServer does not implement Send trait, so the
+    // code has to run on a single thread, but also async and through Tokio.
+
+    let runtime_handle = runtime.handle().clone();
     thread::spawn(move || {
         info!(
             "HTTP server is running on http://{}/ to provide OPC UA server metrics",
             address
         );
-        let sys = actix::System::new("http-server");
-        let addr = server::new(move || {
-            App::with_state(HttpState {
-                server_state: server_state_http.clone(),
-                connections: connections.clone(),
-                server_metrics: server_metrics.clone(),
+
+        let local = tokio::task::LocalSet::new();
+        local.spawn_local(async move {
+            // Spawns a new HTTP server
+            if let Ok(server) = HttpServer::new(move || {
+                App::new()
+                    .app_data(web::Data::new(AppState {
+                        server_state: server_state_http.clone(),
+                        connections: connections.clone(),
+                        server_metrics: server_metrics.clone(),
+                        base_path: Arc::new(RwLock::new(base_path.clone())),
+                    }))
+                    .route("/server/metrics", web::get().to(metrics))
+                    .route("/server/abort", web::get().to(abort))
+                    .route("/", web::get().to(index))
             })
-            .resource("/server/metrics", |r| {
-                r.method(http::Method::GET).f(metrics)
-            })
-            .resource("/server/abort", |r| r.method(http::Method::GET).f(abort))
-            .handler(
-                "/",
-                fs::StaticFiles::new(base_path.clone())
-                    .unwrap()
-                    .index_file("index.html"),
-            )
-        })
-        .bind(&address)
-        .unwrap()
-        .start();
-
-        // Give the address info to the quit task
-        let _ = tx.send(addr);
-
-        // Run
-        let _ = sys.run();
-    });
-
-    // Get the address info from the http server thread
-    let addr = rx.recv().unwrap();
-
-    // Spawn tokio to monitor for quit and to shutdown the http server
-    thread::spawn(move || {
-        tokio::runtime::Builder::new_current_thread()
-            .enable_all()
-            .build()
-            .unwrap()
-            .block_on(async move {
-                let mut timer = interval_at(Instant::now(), Duration::from_secs(1));
-                loop {
-                    {
-                        let server_state = trace_read_lock!(server_state);
-                        if server_state.is_abort() {
-                            let _ = addr.send(server::StopServer { graceful: false });
-                            info!("HTTP server will be stopped");
-                            break;
-                        }
-                    }
-                    timer.tick().await;
-                }
-            });
+            .bind(&address)
+            {
+                let _ = server.run().await;
+            } else {
+                error!("Could not start HTTP server");
+            }
+        });
+        runtime_handle.block_on(local);
+        debug!("HTTP server has terminated");
     });
 }
