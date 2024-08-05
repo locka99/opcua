0596ff6cf5af9e9e591d4a4db4218d93a8403782
Adam Lock <locka99@gmail.com>
2024-08-05 11:25:42 +0100

    DataSetWriterId may be a string or an int

--- lib/src/pubsub/json/data_set_message.rs
+++ lib/src/pubsub/json/data_set_message.rs
@@ -5,6 +5,7 @@
 use std::collections::HashMap;
 use std::fmt;
 
+use serde::de::Error;
 use serde::{
     de::{self, MapAccess},
     Deserialize, Deserializer, Serialize, Serializer,
@@ -100,6 +101,74 @@ impl<'de> serde::de::Visitor<'de> for PayloadVisitor {
 
 pub type Payload = HashMap<String, PayloadValue>;
 
+#[derive(Debug, PartialEq)]
+enum DataSetWriterId {
+    String(String),
+    UInt16(u16),
+}
+
+impl Default for DataSetWriterId {
+    fn default() -> Self {
+        DataSetWriterId::UInt16(0)
+    }
+}
+
+impl Serialize for DataSetWriterId {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        match self {
+            DataSetWriterId::UInt16(value) => value.serialize(serializer),
+            DataSetWriterId::String(value) => value.serialize(serializer),
+        }
+    }
+}
+
+impl<'de> Deserialize<'de> for DataSetWriterId {
+    fn deserialize<D>(deserializer: D) -> Result<DataSetWriterId, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        deserializer.deserialize_any(DataSetWriterIdVisitor)
+    }
+}
+
+struct DataSetWriterIdVisitor;
+
+impl<'de> serde::de::Visitor<'de>
+    for crate::pubsub::json::data_set_message::DataSetWriterIdVisitor
+{
+    type Value = DataSetWriterId;
+
+    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        write!(formatter, "a dataset id")
+    }
+
+    fn visit_u16<E>(self, v: u16) -> Result<Self::Value, E> {
+        Ok(Self::Value::UInt16(v))
+    }
+
+    fn visit_string<E>(self, v: String) -> Result<Self::Value, E>
+    where
+        E: Error,
+    {
+        Ok(Self::Value::String(v))
+    }
+}
+
+impl From<u16> for DataSetWriterId {
+    fn from(value: u16) -> Self {
+        DataSetWriterId::UInt16(value)
+    }
+}
+
+impl From<String> for DataSetWriterId {
+    fn from(value: String) -> Self {
+        DataSetWriterId::String(value)
+    }
+}
+
 /// JSON DataSetMessage definition.
 ///
 /// Optional fields are determined by DataSetMessageContentMask
@@ -108,8 +177,12 @@ pub type Payload = HashMap<String, PayloadValue>;
 pub struct DataSetMessage {
     /// An identifier for the dataset writer which created the DataSetMessage. This value is unique
     /// within the scope of the publisher.
-    /// TODO Json mapping for this field is UInt16
-    pub data_set_writer_id: String,
+    pub data_set_writer_id: DataSetWriterId,
+    // data_set_writer_name: String,
+    // publisher_id: String,
+    // writer_group_name: String:
+    // minor_version: Option<u16>
+    // message_type: String
     /// Strictly monotonically increasing sequence number assigned to the DataSetMessage by the data set writer.
     #[serde(skip_serializing_if = "Option::is_none")]
     #[serde(default)]
@@ -136,9 +209,12 @@ pub struct DataSetMessage {
 impl core::DataSetMessage for DataSetMessage {}
 
 impl DataSetMessage {
-    pub fn new(data_set_writer_id: String, payload: Payload) -> Self {
+    pub fn new<V>(data_set_writer_id: V, payload: Payload) -> Self
+    where
+        V: Into<DataSetWriterId>,
+    {
         Self {
-            data_set_writer_id,
+            data_set_writer_id: data_set_writer_id.into(),
             payload,
             ..Default::default()
         }
@@ -147,50 +223,67 @@ impl DataSetMessage {
 
 #[test]
 fn serialize() {
-    let msg1 = DataSetMessage {
-        data_set_writer_id: "Writer_Id_1".into(),
+    let now = DateTime::now();
+
+    let expected = DataSetMessage {
+        data_set_writer_id: 5555.into(),
         sequence_number: Some(1234),
         meta_data_version: Some(ConfigurationVersionDataType {
             major_version: 1001,
             minor_version: 2002,
         }),
-        timestamp: Some(DateTime::rfc3339_now()),
+        timestamp: Some(now),
         status: Some(StatusCode::BadViewIdUnknown),
         payload: Payload::new(),
     };
 
     // Serialize, deserialize, compare to original
-    let v = serde_json::to_string(&msg1).unwrap();
+    let expected_str = serde_json::to_string(&expected).unwrap();
+    println!("JSON from serializing == {}", expected_str);
+    let expected_value = serde_json::to_value(&expected).unwrap();
 
-    let now = DateTime::now();
     let now_str = now.to_string();
 
-    println!("JSON from serializing == {}", v);
+    let actual = json!(
+        {
+            "DataSetWriterId": 5555,
+            "SequenceNumber": 1234,
+            "MetaDataVersion": {
+                "MajorVersion": 1001,
+                "MinorVersion": 2002,
+            },
+            "Timestamp": now_str,
+            "Status": 2154496000u32,
+            "Payload": {}
+        }
+    );
+
+    assert_eq!(actual, expected_value);
 
     // Test for field/value expected / unexpected
-    assert!(v.contains("DataSetWriterId"));
-    assert!(v.contains("Writer_Id_1"));
+    assert!(expected_str.contains("DataSetWriterId"));
+    assert!(expected_str.contains("Writer_Id_1"));
 
-    assert!(v.contains("SequenceNumber"));
-    assert!(v.contains("1234"));
+    assert!(expected_str.contains("SequenceNumber"));
+    assert!(expected_str.contains("1234"));
 
-    assert!(v.contains("Timestamp"));
+    assert!(expected_str.contains("Timestamp"));
     // TODO expected time format assert!(v.contains(now_str));
 
-    assert!(v.contains("MetaDataVersion"));
-    assert!(v.contains("MajorVersion"));
-    assert!(v.contains("1001"));
-    assert!(v.contains("MinorVersion"));
-    assert!(v.contains("2002"));
+    assert!(expected_str.contains("MetaDataVersion"));
+    assert!(expected_str.contains("MajorVersion"));
+    assert!(expected_str.contains("1001"));
+    assert!(expected_str.contains("MinorVersion"));
+    assert!(expected_str.contains("2002"));
 
-    assert!(v.contains("Status"));
-    assert!(v.contains("2154496000")); // Hex 0x806B_0000 as decimal
+    assert!(expected_str.contains("Status"));
+    assert!(expected_str.contains("2154496000")); // Hex 0x806B_0000 as decimal
 
-    assert!(v.contains("Payload"));
+    assert!(expected_str.contains("Payload"));
 
-    let msg2 = serde_json::from_str(&v).unwrap();
+    let actual2 = serde_json::from_str(&expected_str).unwrap();
 
-    assert_eq!(msg1, msg2)
+    assert_eq!(expected, actual2)
 }
 
 #[test]
@@ -261,7 +354,7 @@ fn deserialize() {
         "Payload": {}
     });
     let v: DataSetMessage = serde_json::from_value(in1).unwrap();
-    assert!(v.data_set_writer_id.is_empty());
+    assert_eq!(v.data_set_writer_id, DataSetWriterId::String(String::new()));
     assert!(v.meta_data_version.is_none());
     assert!(v.timestamp.is_none());
     assert!(v.status.is_none());
